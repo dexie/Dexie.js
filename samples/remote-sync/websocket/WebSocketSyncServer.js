8680b9679de10b7231476144c2e0dd10daf7a770
David Fahlander <david.fahlander@gmail.com>
2014-06-12 13:04:03 +0200

    Dexie.Syncable: Corrected merge algorithm
    When merging two update-changes, we didnt handle the case when the
    previous change contained a parent key to the next change's keyPath.
    What would happen before is that the merged change would contain both
    the parent keyPath and the sub keyPath. When this change would have been
    applied, it would still be OK if we were certain that the parent path
    would apply first and sub-path second, but that's not how it is
    implemented.
    
    To canonicalize the change, I added a check for parent path and if it
    exists, let the new change apply to the contents of the prev change
    instead.

--- samples/remote-sync/websocket/WebSocketSyncServer.js
+++ samples/remote-sync/websocket/WebSocketSyncServer.js
@@ -392,8 +392,16 @@
     function combineUpdateAndUpdate(prevChange, nextChange) {
         var clonedChange = deepClone(prevChange); // Clone object before modifying since the earlier change in db.changes[] would otherwise be altered.
         Object.keys(nextChange.mods).forEach(function (keyPath) {
-            // Add or replace this keyPath and its new value
-            clonedChange.mods[keyPath] = nextChange.mods[keyPath];
+            // If prev-change was changing a parent path of this keyPath, we must update the parent path rather than adding this keyPath
+            var hadParentPath = false;
+            Object.keys(prevChange.mods).filter(function (parentPath) { return keyPath.indexOf(parentPath + '.') === 0 }).forEach(function (parentPath) {
+                setByKeyPath(clonedChange[parentPath], keyPath.substr(parentPath.length + 1), nextChange.mods[keyPath]);
+                hadParentPath = true;
+            });
+            if (!hadParentPath) {
+                // Add or replace this keyPath and its new value
+                clonedChange.mods[keyPath] = nextChange.mods[keyPath];
+            }
             // In case prevChange contained sub-paths to the new keyPath, we must make sure that those sub-paths are removed since
             // we must mimic what would happen if applying the two changes after each other:
             Object.keys(prevChange.mods).filter(function (subPath) { return subPath.indexOf(keyPath + '.') === 0 }).forEach(function (subPath) {
