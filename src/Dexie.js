e053b9df5e1ba5bcd253d355aa82a923ec7f4546
David Fahlander <david.fahlander@gmail.com>
2014-10-27 18:29:14 +0100

    Releasing version 1.0
    ...containing the collection of bugfixes the last 3 months:
    
    Bugfix unique+compound indexes (Pull request #19, Oct 17)
    Bugfixed unit tests (Aug 30)
    Bugfix chained listeners on('ready') (Aug 10)
    Sub-transaction bugfixes (Aug 1)

--- src/Dexie.js
+++ src/Dexie.js
@@ -3,7 +3,7 @@
 
    By David Fahlander, david.fahlander@gmail.com
 
-   Version 0.9.9.1 (alpha - not yet distributed) - DATE, YEAR.
+   Version 1.0.0.1 (alpha - not yet distributed) - DATE, YEAR.
 
    Tested successfully on Chrome, IE, Firefox and Opera.
 
@@ -2991,7 +2991,7 @@
     }
 
     // API Version Number: Type Number, make sure to always set a version number that can be comparable correctly. Example: 0.9, 0.91, 0.92, 1.0, 1.01, 1.1, 1.2, 1.21, etc.
-    Dexie.version = 0.991;
+    Dexie.version = 1.01;
 
 
 
