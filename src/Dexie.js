f24e0550a07bba359b1bf77777b42d5c93613cd0
David Fahlander <david.fahlander@gmail.com>
2014-12-09 03:07:45 +0100

    Another semicolon fix

--- src/Dexie.js
+++ src/Dexie.js
@@ -2123,7 +2123,7 @@
 
 
         function combine(filter1, filter2) {
-            return filter1 ? filter2 ? function () { return filter1.apply(this, arguments) && filter2.apply(this, arguments) } : filter1 : filter2;
+            return filter1 ? filter2 ? function () { return filter1.apply(this, arguments) && filter2.apply(this, arguments); } : filter1 : filter2;
         }
 
         function hasIEDeleteObjectStoreBug() {
