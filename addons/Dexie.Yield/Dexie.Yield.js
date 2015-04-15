348c5798fff2b965b06253a71f228dc3d188874c
David Fahlander <david.fahlander@gmail.com>
2015-04-15 09:42:56 +0200

    Made async() independant on Dexie.Promise
    This async() / spawn() solution is Promise implementation agnostic in a
    deeper level than Task.js and the existing proposals for ecmascript 7
    async/await. The reason is that it adapts to the yielded Promise instead
    of always converting it to the browser-built-in Promise, with one
    exception: If the generator function does not ever use yield, we have to
    encapsulate the return value into a Promise. This commit will use a
    standard Promise if that occurs instead of returning a Dexie.Promise.
    The reason is just to make it more clear that the implementation of
    async() and spawn() in it's whole is not a Dexie-specific solution but a
    generic solution that would be good to be adopted by the standard makers
    of ES7.

--- addons/Dexie.Yield/Dexie.Yield.js
+++ addons/Dexie.Yield/Dexie.Yield.js
@@ -2,6 +2,53 @@
 
     define('Dexie.Yield', ["Dexie"], function (Dexie) {
 
+        function spawn(generatorFn) {
+            return iterate(generatorFn());
+        }
+
+        function async(generatorFn) {
+            return function () {
+                return iterate(generatorFn.apply(this, arguments));
+            }
+        }
+        
+        function iterate (iterable) {
+            var callNext = function (result) { return iterable.next(result); },
+                doThrow = function (error) { return iterable.throw(error); },
+                onSuccess = step(callNext),
+                onError = step(doThrow);
+
+            function step(getNext, initial) {
+                return function (val) {
+                    var next = getNext(val);
+                    if (next.done) {
+                        if (next.value && typeof next.value.then === 'function')
+                            // Emphasize using "return yield <promise>;" instead of just "return <promise>;".
+                            return Promise.reject(new TypeError("Illegal to return a promise without yield"));
+
+                        // Promise.resolve() only needed when no yield has been used at all.
+                        // Try not to use Promise.resolve() unless needed, because it will convert
+                        // the returned Promise implementation to the default Promise implementation and
+                        // the user could not get the features and benefits of various Promise implementations.
+                        return initial ? Promise.resolve(next.value) : next.value;
+                    }
+
+                    if (!next.value || typeof next.value.then !== 'function')
+                        // Don't accept yielding a non-promise such as "yield 3;".
+                        // By not accepting that, we could detect bugs better.
+                        iterable.throw(new TypeError("Only acceptable to yield a Promise"));
+
+                    return next.value.then(onSuccess, onError);
+                }
+            }
+
+            try {
+                return step(callNext, true)();
+            } catch (e) {
+                return Promise.reject(e);
+            }
+        }
+
         function DexieYield(db) {
             db.transaction = Dexie.override(db.transaction, function (origDbTransaction) {
                 return function () {
@@ -20,54 +67,6 @@
                     return origDbTransaction.apply(this, arguments);
                 }
             });
-        }
-
-        function iterate(iter) {
-            var Promise = Dexie.Promise,
-                callNext = function (result) { return iter.next(result); },
-                doThrow = function (error) { return iter.throw(error); },
-                onSuccess = step(callNext),
-                onError = step(doThrow);
-
-            function step(getNext, initial) {
-                return function (val) {
-                    var next = getNext(val);
-                    if (next.done) {
-                        if (next.value && typeof next.value.then === 'function')
-                            // Emphasize using "return yield <promise>;" instead of just "return <promise>;".
-                            return Promise.reject(new TypeError("Illegal to return a promise without yield"));
-
-                        // Promise.resolve() only needed when no yield has been used at all.
-                        // Try not to use Promise.resolve() unless needed, because it will convert
-                        // the returned Promise implementation to our Promise implementation and
-                        // the user could not get the features and benefits of various Promise implementations.
-                        return initial ? Promise.resolve(next.value) : next.value;
-                    }
-
-                    if (!next.value || typeof next.value.then !== 'function')
-                        // Don't accept yielding a non-promise such as "yield 3;".
-                        // By not accepting that, we could detect bugs better.
-                        iter.throw(new TypeError("Only acceptable to yield a Promise"));
-
-                    return next.value.then(onSuccess, onError);
-                }
-            }
-
-            try {
-                return step(callNext, true)();
-            } catch (e) {
-                return Promise.reject(e);
-            }
-        }
-
-        function spawn(generatorFn) {
-            return iterate(generatorFn());
-        }
-
-        function async(generatorFn) {
-            return function() {
-                return iterate(generatorFn.apply(this, arguments));
-            }
         }
 
         DexieYield.async = async;
