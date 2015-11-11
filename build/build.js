af6ee55c2127e5fac467efe04b18a9a3d264b500
David Fahlander <david.fahlander@gmail.com>
2015-11-11 09:52:36 +0100

    Removing node_modules from repo. Moved dist/latest to dist.

--- build/build.js
+++ build/build.js
@@ -1,63 +1,36 @@
 var fs = require('fs');
 var rollup = require('rollup');
 var uglify = require("uglify-js");
+var utils = require('./build-utils');
 
-copyFile("src/Dexie.js", "dist/latest/Dexie.es6.js", function (err) {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    rollup.rollup({
+utils.copyFiles({
+    "src/Dexie.js": "dist/Dexie.es6.js",
+    "src/Dexie.d.ts": "dist/Dexie.d.ts"
+}).then(() => {
+    console.log("Reading and bundling source <= \tsrc/Dexie.js");
+    return rollup.rollup({
         // The bundle's starting point. This file will be
         // included, along with the minimum necessary code
         // from its dependencies
         entry: 'src/Dexie.js'
-    }).then(function (bundle) {
-        // Generate bundle + sourcemap
-        return bundle.write({
-            format: 'umd',
-            dest: 'dist/latest/Dexie.js',
-            sourceMap: true,
-            moduleName: 'Dexie',
-            sourceMapRelativePaths: true
-        });
-    }).then(function () {
-        console.log("dsd222");
-        var result = uglify.minify("dist/latest/Dexie.js", {
-            inSourceMap: "dist/latest/Dexie.js.map",
-            outSourceMap: "Dexie.min.js.map"
-        });
-        console.log("slkajfk");
-        //console.log("Hello" + result.code);
-        fs.writeFileSync('dist/latest/Dexie.min.js', result.code);
-        console.log("slkajfk 222");
-        fs.writeFileSync('dist/latest/Dexie.min.js.map', result.map);
-        console.log("slkajfk 33");
-    }).catch(function (err) {
-        console.error(err);
     });
-});
+}).then(bundle => {
+    // Generate bundle + sourcemap
+    console.log('Writing UMD bundle to =>\tdist/Dexie.js');
+    return bundle.write({
+        format: 'umd',
+        dest: 'dist/Dexie.js',
+        sourceMap: true,
+        moduleName: 'Dexie',
+        sourceMapRelativePaths: true
+    });
+}).then(() => {
+    console.log("Minifying the ES5 UMD bundle =>\tdist/Dexie.min.js, (including .map)");
+    var result = uglify.minify("dist/Dexie.js", {
+        inSourceMap: "dist/Dexie.js.map",
+        outSourceMap: "Dexie.min.js.map"
+    });
 
-function copyFile(source, target, cb) {
-    var cbCalled = false;
-
-    var rd = fs.createReadStream(source);
-    rd.on("error", function (err) {
-        done(err);
-    });
-    var wr = fs.createWriteStream(target);
-    wr.on("error", function (err) {
-        done(err);
-    });
-    wr.on("close", function (ex) {
-        done();
-    });
-    rd.pipe(wr);
-
-    function done(err) {
-        if (!cbCalled) {
-            cb(err);
-            cbCalled = true;
-        }
-    }
-}
+    fs.writeFileSync('dist/Dexie.min.js', result.code);
+    fs.writeFileSync('dist/Dexie.min.js.map', result.map);
+}).catch(err => console.error(err));
--- node_modules/rollup/src/utils/object.js
+++
@@ -1,5 +0,0 @@
-export const keys = Object.keys;
-
-export function blank () {
-	return Object.create( null );
-}
