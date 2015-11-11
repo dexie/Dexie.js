5ac6a367431a58f6a1b997418a13617212372a24
David Fahlander <david.fahlander@gmail.com>
2015-11-11 09:55:11 +0100

    Updated gitignore and added missing file build-utils

---
+++ build/build-utils.js
@@ -0,0 +1,30 @@
+﻿var fs = require('fs');
+
+function copyFile(source, target) {
+    console.log('Copying '+source+' => \t'+target);
+    return new Promise(function (resolve, reject) {
+        var rd = fs.createReadStream(source);
+        rd.on("error", function (err) {
+            reject(err);
+        });
+        var wr = fs.createWriteStream(target);
+        wr.on("error", function (err) {
+            reject(err);
+        });
+        wr.on("close", function (ex) {
+            resolve();
+        });
+        rd.pipe(wr);
+    });
+}
+
+function copyFiles (sourcesAndTargets) {
+    return Promise.all(
+        Object.keys(sourcesAndTargets)
+            .map(source => copyFile(source, sourcesAndTargets[source])));
+}
+
+module.exports = {
+    copyFile: copyFile,
+    copyFiles: copyFiles
+};
