649d5fdb6ce639a619eae4f3ddf8f881fd8fa090
David Fahlander <david.fahlander@gmail.com>
2015-11-17 13:15:14 +0100

    npm package prepared for dexie-yield module.

---
+++ addons/dexie-yield/dist/dexie-yield.js
@@ -0,0 +1,79 @@
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('dexie')) :
+    typeof define === 'function' && define.amd ? define(['exports', 'dexie'], factory) :
+    factory((global.Dexie.Yield = {}),global.Dexie);
+}(this, function (exports,Dexie) { 'use strict';
+
+    Dexie = 'default' in Dexie ? Dexie['default'] : Dexie;
+
+    var Promise = Dexie.Promise;
+
+    function spawn(generatorFn) {
+        return iterate(generatorFn());
+    }
+
+    function async(generatorFn) {
+        return function () {
+            return iterate(generatorFn.apply(this, arguments));
+        }
+    }
+
+    function iterate (iterable) {
+        var callNext = function (result) { return iterable.next(result); },
+            doThrow = function (error) { return iterable.throw(error); },
+            onSuccess = step(callNext),
+            onError = step(doThrow);
+
+        function step(getNext, initial) {
+            return function (val) {
+                var next = getNext(val);
+                if (next.done)
+                    return initial ? Promise.resolve(next.value) : next.value;
+
+                if (!next.value || typeof next.value.then !== 'function')
+                    if (Array.isArray(next.value))
+                        return Promise.all(next.value).then(onSuccess, onError);
+                    else
+                        return Promise.resolve(next.value).then(onSuccess, onError);
+                return next.value.then(onSuccess, onError);
+            }
+        }
+
+        try {
+            return step(callNext, true)();
+        } catch (e) {
+            return Promise.reject(e);
+        }
+    }
+
+    Dexie.addons.push(function (db) {
+        //
+        // Adjust db.transaction() to handle iterable functions as async without explicitely having to write async() around the transaction scope func:
+        //  db.transaction('rw', db.friends, async(function*(){})); - not needed.
+        //  db.transaction('rw', db.friends, function*(){}); // prettier.
+        //
+        db.transaction = Dexie.override(db.transaction, function (origDbTransaction) {
+            return function () {
+                var scopeFunc = arguments[arguments.length - 1];
+
+                function proxyScope() {
+                    var rv = scopeFunc.apply(this, arguments);
+                    if (!rv.next || !rv.throw) return rv; // Not an iterable
+                    return iterate(rv);
+                }
+
+                proxyScope.toString = function () {
+                    return scopeFunc.toString(); // Because original db.transaction may use fn.toString() when error occur.
+                }
+                arguments[arguments.length - 1] = proxyScope;
+                return origDbTransaction.apply(this, arguments);
+            }
+        });
+    });
+
+    exports.spawn = spawn;
+    exports.async = async;
+    exports.iterate = iterate;
+
+}));
+//# sourceMappingURL=dexie-yield.js.map
