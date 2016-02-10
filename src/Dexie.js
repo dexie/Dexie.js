b5fdd2aa1b5806af2696aba96d1053c7dbd50497
David Fahlander <david.fahlander@gmail.com>
2016-02-10 12:24:45 +0100

    Added newlines in header comment

--- src/Dexie.js
+++ src/Dexie.js
@@ -5,7 +5,9 @@
  * By David Fahlander, david.fahlander@gmail.com
  *
  * Version {version}, {date}
+ *
  * www.dexie.com
+ *
  * Apache License Version 2.0, January 2004, http://www.apache.org/licenses/
  */
 
