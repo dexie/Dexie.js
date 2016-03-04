33d59cff5eb54dcb7af60e0dc3c512d1971e8a8e
David Fahlander <david.fahlander@gmail.com>
2016-03-04 16:55:12 +0100

    Upgraded the typescript sample to support Typescript 1.8 and Dexie 1.3. Also included complete build scripts and correct dependencies.

---
+++ samples/typescript/README.md
@@ -0,0 +1,38 @@
+﻿# Sample - Using Dexie.js with Typescript.
+
+This is a sample on how to use Dexie.js with Typescript and babel. The following features are shown:
+
+* How to subclass Dexie and define tables in a type-safe manner.
+* How to create an entity with Dexie.
+* How to use async / await with Dexie.
+* How to create something similar to navigation properties on entities.
+
+## Install
+Start a root / admin shell and write:
+sudo npm install  (on windows, run "npm install" as admin in this directory)
+
+## Build
+npm run build
+
+## Run
+npm install -g http-server
+http-server . -a localhost -p 8081
+Surf to http://localhost:8081/app.html
+
+# The app
+The application stores a simple contact database using a relational database model, where each contact can have
+0..n emails, 0..n phone numbers. Email- and Phone entries have their own tables and reference Contact through
+the contactId index.
+
+The AppDatabase class extends Dexie with the three tables **contacts**, **emails** and **phones**.
+The tables are mapped to typescript classes and interfaces.
+
+The sample shows how to subclass Dexie in typescript and define the tables.
+
+It also shows how to map a typescript class to a database table and call methods on database objects.
+
+# The build system
+npm run build will transpile the code in three steps:
+1. tsc      (typescript -&gt; es6)
+2. babel    (es6 -&gt; es5 commonJs)
+3. webpack  (commonJS -&gt; plain monolit minified js)
---
+++ samples/typescript/src/appdb.ts
@@ -0,0 +1,107 @@
+﻿import Dexie from 'dexie';
+const all = Dexie.Promise.all;
+
+export class AppDatabase extends Dexie {
+
+    contacts: Dexie.Table<Contact, number>;
+    emails: Dexie.Table<IEmailAddress, number>;
+    phones: Dexie.Table<IPhoneNumber, number>;
+
+    constructor() {
+
+        super("ContactsDatabase");
+
+        var db = this;
+
+        //
+        // Define tables and indexes
+        //
+        db.version(1).stores({
+            contacts: '++id, firstName, lastName',
+            emails: '++id, contactId, type, email',
+            phones: '++id, contactId, type, phone',
+        });
+
+        // Let's physically map Contact class to contacts table.
+        // This will make it possible to call loadEmailsAndPhones()
+        // directly on retrieved database objects.
+        db.contacts.mapToClass(Contact);
+    }
+}
+
+/* Just for code completion and compilation - defines
+    * the interface of objects stored in the emails table.
+    */
+export interface IEmailAddress {
+    id?: number;
+    contactId: number;
+    type: string;
+    email: string;
+}
+
+/* Just for code completion and compilation - defines
+    * the interface of objects stored in the phones table.
+    */
+export interface IPhoneNumber {
+    id?: number;
+    contactId: number;
+    type: string;
+    phone: string;
+}
+
+/* This is a 'physical' class that is mapped to
+    * the contacts table. We can have methods on it that
+    * we could call on retrieved database objects.
+    */
+export class Contact {
+    id: number;
+    firstName: string;
+    lastName: string;
+    emails: IEmailAddress[];
+    phones: IPhoneNumber[];
+    
+    constructor(first: string, last: string, id?:number) {
+        this.firstName = first;
+        this.lastName = last;
+        if (id) this.id = id;
+        // Define navigation properties.
+        // Making them non-enumerable will prevent them from being handled by indexedDB
+        // when doing put() or add().
+        Object.defineProperties(this, {
+            emails: {value: [], enumerable: false, writable: true },
+            phones: {value: [], enumerable: false, writable: true }
+        });
+    }
+    
+    async loadNavigationProperties() {
+        [this.emails, this.phones] = await all<any>(
+            db.emails.where('contactId').equals(this.id).toArray(),
+            db.phones.where('contactId').equals(this.id).toArray()
+        );
+    }
+
+    save() {
+        return db.transaction('rw', db.contacts, db.emails, db.phones, async () => {
+            
+            let [emailIds, phoneIds] = await all (
+                // Save existing arrays
+                all(this.emails.map(email => db.emails.put(email))),
+                all(this.phones.map(phone => db.phones.put(phone))));
+                
+            // Remove items from DB that is was not saved here:
+            await db.emails.where('contactId').equals(this.id)
+                .and(email => emailIds.indexOf(email.id) === -1)
+                .delete();
+
+            await db.phones.where('contactId').equals(this.id)
+                .and(phone => phoneIds.indexOf(phone.id) === -1)
+                .delete();
+
+            // At last, save this Contact
+            return await db.contacts.put(this);
+        });
+    }
+}
+
+export var db = new AppDatabase();
+
