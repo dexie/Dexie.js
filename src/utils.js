3293e434b76b578c844a8f32e7f8fb9f1f535023
David Fahlander <david.fahlander@gmail.com>
2016-03-17 13:11:13 +0100

    Refactor bug.

--- src/utils.js
+++ src/utils.js
@@ -8,6 +8,7 @@
  * www.dexie.com
  * Apache License Version 2.0, January 2004, http://www.apache.org/licenses/
  */
+import Promise from './Promise';
 
 export var keys = Object.keys;
 export var isArray = Array.isArray;
