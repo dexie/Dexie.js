92d252814560f04e35bae45bccee35814f5cc9e5
David Fahlander <david.fahlander@gmail.com>
2016-06-09 11:51:28 +0200

    Revert "Safari friendlier maxKey."
    This reverts commit 01065a6521bae39c0ac758a5ee4f4d29c95ee21b.

--- src/Dexie.js
+++ src/Dexie.js
@@ -58,7 +58,7 @@ var DEXIE_VERSION = '{version}',
     maxString = String.fromCharCode(65535),
     // maxKey is an Array<Array> if indexedDB implementations supports array keys (not supported by IE,Edge or Safari at the moment)
     // Otherwise maxKey is maxString. This is handy when needing an open upper border without limit.
-    maxKey = (function(){try {IDBKeyRange.only([[]]);return [maxString];}catch(e){return maxString;}})(),
+    maxKey = (function(){try {IDBKeyRange.only([[]]);return [[]];}catch(e){return maxString;}})(),
     INVALID_KEY_ARGUMENT = "Invalid key provided. Keys must be of type string, number, Date or Array<string | number | Date>.",
     STRING_EXPECTED = "String expected.",
     connections = [],
