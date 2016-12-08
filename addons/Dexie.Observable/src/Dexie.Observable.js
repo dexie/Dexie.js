f6874b02fabbe0866c358a15e3ac7533aceb848a
David Fahlander <david.fahlander@gmail.com>
2016-12-08 14:12:30 +0100

    Better scheduling of old changes cleanup

--- addons/Dexie.Observable/src/Dexie.Observable.js
+++ addons/Dexie.Observable/src/Dexie.Observable.js
@@ -827,20 +827,27 @@ Observable.deleteOldChanges = function(db) {
     // This is a background job and should never be done within
     // a caller's transaction. Use Dexie.ignoreTransaction() to ensure that.
     // We should not return the Promise but catch it ourselves instead.
+
+    // To prohibit starving the database we want to lock transactions as short as possible
+    // and since we're not in a hurry, we could do this job in chunks and reschedule a 
+    // continuation every 500 ms.
+    const CHUNK_SIZE = 100;
+
     Dexie.ignoreTransaction(()=>{
         return db._syncNodes.orderBy("myRevision").first(oldestNode => {
-            let timeout = Date.now() + 300,
-                hasTimedOut = false,
-                timedout = () => (hasTimedOut = (Date.now() > timeout));
-
             return db._changes
                 .where("rev").below(oldestNode.myRevision)
-                .until(timedout)
-                .delete()
-                .then(() => {
-                    // If not done garbage collecting, reschedule a continuation of it until done.
-                    if (hasTimedOut) setTimeout(() => db.isOpen() && Observable.deleteOldChanges(db), 300);
-                });
+                .limit(CHUNK_SIZE)
+                .primaryKeys();
+        }).then(keysToDelete => {
+            if (keysToDelete.length === 0) return; // Done.
+            return db._changes.bulkDelete(keysToDelete).then(()=> {
+                // If not done garbage collecting, reschedule a continuation of it until done.
+                if (keysToDelete.length === CHUNK_SIZE) {
+                    // Limit reached. Changes are there are more job to do. Schedule again:
+                    setTimeout(() => db.isOpen() && Observable.deleteOldChanges(db), 500);
+                }
+            });
         });
     }).catch(()=>{
         // The operation is not cruzial. A failure could almost only be due to that database has been closed.
