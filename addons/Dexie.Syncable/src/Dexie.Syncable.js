1090fbff801d841859201476a59354f4cc92ff06
Nikolas Poniros <nikolas@poniros.de>
2016-12-21 20:42:16 +0100

    Externalize getBaseRevisionAndMaxClientRevision() and write tests

--- addons/Dexie.Syncable/src/Dexie.Syncable.js
+++ addons/Dexie.Syncable/src/Dexie.Syncable.js
@@ -22,6 +22,7 @@ import "dexie-observable";
 
 import combineCreateAndUpdate from './combine-create-and-update.js';
 import combineUpdateAndUpdate from './combine-update-and-update.js';
+import getBaseRevisionAndMaxClientRevision from './get-base-revision-and-max-client-revision.js';
 
 var override = Dexie.override,
     Promise = Dexie.Promise,
@@ -470,30 +471,6 @@ export default function Syncable (db) {
 
             function abortTheProvider(error) {
                 activePeer.disconnect(Statuses.ERROR, error);
-            }
-
-            function getBaseRevisionAndMaxClientRevision(node) {
-                /// <param name="node" type="db.observable.SyncNode"></param>
-                if (node.remoteBaseRevisions.length === 0)
-                    return {
-                        // No remoteBaseRevisions have arrived yet. No limit on clientRevision and provide null as remoteBaseRevision:
-                        maxClientRevision: Infinity,
-                        remoteBaseRevision: null
-                    };
-                for (var i = node.remoteBaseRevisions.length - 1; i >= 0; --i) {
-                    if (node.myRevision >= node.remoteBaseRevisions[i].local) {
-                        // Found a remoteBaseRevision that fits node.myRevision. Return remoteBaseRevision and eventually a roof maxClientRevision pointing out where next remoteBaseRevision bases its changes on.
-                        return {
-                            maxClientRevision: i === node.remoteBaseRevisions.length - 1 ? Infinity : node.remoteBaseRevisions[i + 1].local,
-                            remoteBaseRevision: node.remoteBaseRevisions[i].remote
-                        };
-                    }
-                }
-                // There are at least one item in the list but the server hasnt yet become up-to-date with the 0 revision from client. 
-                return {
-                    maxClientRevision: node.remoteBaseRevisions[0].local,
-                    remoteBaseRevision: null
-                };
             }
 
             function getLocalChangesForNode_autoAckIfEmpty(node, cb) {
--- addons/Dexie.Syncable/test/unit/unit-tests-all.js
+++ addons/Dexie.Syncable/test/unit/unit-tests-all.js
@@ -1,2 +1,3 @@
 import './tests-combine-create-and-update.js';
 import './tests-combine-update-and-update.js';
+import './tests-get-base-revision-and-max-client-revision.js';
