1d51e530fc7ea2cb5cb0a3949fe92fb2d1ba1c75
Nikolas Poniros <nikolas@poniros.de>
2016-12-22 13:41:11 +0100

    Add unit tests for bulk-update

--- addons/Dexie.Syncable/test/unit/unit-tests-all.js
+++ addons/Dexie.Syncable/test/unit/unit-tests-all.js
@@ -1,4 +1,5 @@
 import './tests-apply-changes.js';
+import './tests-bulk-update.js';
 import './tests-combine-create-and-update.js';
 import './tests-combine-update-and-update.js';
 import './tests-get-base-revision-and-max-client-revision.js';
