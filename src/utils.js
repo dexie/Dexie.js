5d6c03bfa8834c2bf7276a5fc4a8020cae0f70c4
David Fahlander <david.fahlander@gmail.com>
2016-12-22 01:59:32 +0100

    Update url in comments from .com to .org

--- src/utils.js
+++ src/utils.js
@@ -5,7 +5,7 @@
  * By David Fahlander, david.fahlander@gmail.com
  *
  * Version {version}, {date}
- * www.dexie.com
+ * www.dexie.org
  * Apache License Version 2.0, January 2004, http://www.apache.org/licenses/
  */
 export var keys = Object.keys;
