6ea67bdbd5b6bcd5127a64855d96bd41b4f19b3e
Nikolas Poniros <nikolas@poniros.de>
2016-12-22 16:55:34 +0100

    Externalize getOrCreateSyncNode + bug fixes
    Have getOrCreateSyncNode and PersistedContext in their own modules.
    Add tests for those.
    
    getOrCreateSyncNode bug fixes:
    
    The function in Dexie.Promise.resolve() was never called meaning that we
    never set node.myRevision to the currentRevision if initialUpload was
    false.
    
    db._changes.lastKey is not a function. Changed that to create a
    collection and call lastKey on that.
    
    Add db._changes to the transaction.
    
    The unit test "should set myRevision to the last _changes if
    initialUpload is false" makes sure that the Dexie.Promise.resolve
    function is called and that node.myRevision is set to the last key in
    changes.

--- addons/Dexie.Syncable/test/unit/unit-tests-all.js
+++ addons/Dexie.Syncable/test/unit/unit-tests-all.js
@@ -3,4 +3,6 @@ import './tests-bulk-update.js';
 import './tests-combine-create-and-update.js';
 import './tests-combine-update-and-update.js';
 import './tests-get-base-revision-and-max-client-revision.js';
+import './tests-get-or-create-sync-node.js';
 import './tests-merge-change.js';
+import './tests-PersistedContext.js';
