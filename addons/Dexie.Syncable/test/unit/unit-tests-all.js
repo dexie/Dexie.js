2570c9bc4911169573c0d12af95b8e4e3d827481
Nikolas Poniros <nikolas@poniros.de>
2016-12-22 13:13:19 +0100

    Add unit test for apply-changes

--- addons/Dexie.Syncable/test/unit/unit-tests-all.js
+++ addons/Dexie.Syncable/test/unit/unit-tests-all.js
@@ -1,3 +1,4 @@
+import './tests-apply-changes.js';
 import './tests-combine-create-and-update.js';
 import './tests-combine-update-and-update.js';
 import './tests-get-base-revision-and-max-client-revision.js';
