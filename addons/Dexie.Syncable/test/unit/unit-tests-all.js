17d26ce40b22529a1bc87d33a2346f487bf46895
Nikolas Poniros <nikolas@poniros.de>
2016-12-23 14:29:04 +0100

    Add tests for finallyCommitAllChanges

--- addons/Dexie.Syncable/test/unit/unit-tests-all.js
+++ addons/Dexie.Syncable/test/unit/unit-tests-all.js
@@ -2,6 +2,7 @@ import './tests-apply-changes.js';
 import './tests-bulk-update.js';
 import './tests-combine-create-and-update.js';
 import './tests-combine-update-and-update.js';
+import './tests-finally-commit-all-changes.js';
 import './tests-get-base-revision-and-max-client-revision.js';
 import './tests-get-or-create-sync-node.js';
 import './tests-merge-change.js';
