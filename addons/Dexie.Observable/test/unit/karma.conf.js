43196b581821a2c20fecab124099e3b63ba8d5b3
Nikolas Poniros <nikolas@poniros.de>
2016-12-29 16:52:03 +0100

    Add karma config and eslint config
    Add karma config for the integration tests
    * travis
    * release
    * main config to include tests
    
    package.json
    * add scripts for tests
    * add scripts for eslint
    
    Add eslint configs for src and unit tests
    
    Update build to build unit tests

---
+++ addons/Dexie.Observable/test/unit/karma.conf.js
@@ -0,0 +1,48 @@
+module.exports = function(config) {
+  const configuration = {
+    basePath: '../../../../',
+
+    frameworks: [
+      'qunit'
+    ],
+
+    reporters: [
+      'mocha'
+    ],
+
+    client: {
+      captureConsole: true
+    },
+
+    files: [
+      './test/babel-polyfill/polyfill.min.js',
+      'node_modules/qunitjs/qunit/qunit.js',
+      'test/karma-env.js',
+      'dist/dexie.js',
+      'addons/Dexie.Observable/test/unit/bundle.js',
+      { pattern: '**/*.map', watched: false, included: false, served: true}
+    ],
+
+    port: 19144,
+    //captureTimeout: 30 * 1000,
+    //browserNoActivityTimeout: 10 * 60 * 1000,
+    colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    //logLevel: config.LOG_DEBUG,
+
+    browsers: [
+        'Chrome'
+    ],
+
+    plugins: [
+      'karma-qunit',
+      'karma-mocha-reporter',
+      'karma-chrome-launcher',
+      //'karma-firefox-launcher'
+    ]
+  };
+
+  config.set(configuration);
+};
