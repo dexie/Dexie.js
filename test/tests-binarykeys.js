8d41fa9113fdc24c5cb2141f4a381c839253ddf4
David Fahlander <david.fahlander@gmail.com>
2017-08-30 10:12:03 +0200

    Avoid Firefox 55 bugs in the unit tests for binary keys I've found a bug in FF55 where it wont allow calling IDBObjectStore.get() or IDBObjectStore.delete() passing a binary key as argument. This has been reported as issue https://bugzilla.mozilla.org/show_bug.cgi?id=1395071
    The binary keys tests now avoids calling these methods. They must also cleanup the added items in the tables as the test runner would otherwise stumble on FF bugs when resetting the database between tests.

--- test/tests-binarykeys.js
+++ test/tests-binarykeys.js
@@ -25,24 +25,54 @@ module("binarykeys", {
     }
 });
 
-promisedTest('binaryPrimaryKey', async () => {
+promisedTest('Binary Primary Key (Int32Array)', async () => {
     if (!supports("binarykeys")) {
         ok(true, "This browser does not support IndexedDB 2.0");
         return;
     }
-    const id = new Float32Array([4.3, 2.5]);
-    equal (Math.round(id[0] * 100), 4.3 * 100, "Sanity check 1");
-    equal (Math.round(id[1] * 100), 2.5 * 100, "Sanity check 2");
-    
-    await db.items.add({id, data: "string"});
+    try {
+        const id = new Int32Array([4, 2]);
+        equal (id[0], 4, "Sanity check 1");
+        equal (id[1], 2, "Sanity check 2");
+        
+        await db.items.add({id, data: "string"});
 
-    let back = await db.items.get(new Float32Array([4.3, 2.5]));
-    equal (back.data, "string", "Should retrieve an object by its binary primary key");
-    equal (Math.round(back.id[0] * 100), 4.3 * 100, "Should get correct float value 4.3");
-    equal (Math.round(back.id[1] * 100), 2.5 * 100, "Should get correcg float value 2.5");
+        let back = await db.items.where({id: new Int32Array([4, 2])}).first();
+        equal (back.data, "string", "Should retrieve an object by its binary primary key");
+        equal (back.id[0], 4, "Should get correct value 4");
+        equal (back.id[1], 2, "Should get correcg value 2");
+    } finally {
+        // Cleanup. This is only needed because of a firefox bug: https://bugzilla.mozilla.org/show_bug.cgi?id=1395071
+        await db.items.clear();
+    }
 });
 
-promisedTest('binaryIndex', async () => {
+promisedTest('Binary Primary Key (Float32Array)', async () => {
+    if (!supports("binarykeys")) {
+        ok(true, "This browser does not support IndexedDB 2.0");
+        return;
+    }
+    try {
+        const id = new Float32Array([4.3, 2.5]);
+        equal (Math.round(id[0] * 100), 4.3 * 100, "Sanity check 1");
+        equal (Math.round(id[1] * 100), 2.5 * 100, "Sanity check 2");
+        
+        await db.items.add({id, data: "string"});
+
+        let back = await db.items // avoiding db.items.get(key) because it triggers bug in Firefox 55.
+            .where({id: new Float32Array([4.3, 2.5])})
+            .first();
+        equal (back.data, "string", "Should retrieve an object by its binary primary key");
+        equal (Math.round(back.id[0] * 100), 4.3 * 100, "Should get correct float value 4.3");
+        equal (Math.round(back.id[1] * 100), 2.5 * 100, "Should get correcg float value 2.5");
+    } finally {
+        // Cleanup. This is only needed because of a firefox bug: https://bugzilla.mozilla.org/show_bug.cgi?id=1395071
+        await db.items.clear();        
+    }
+});
+
+
+promisedTest('Binary Index', async () => {
     if (!supports("binarykeys")) {
         ok(true, "This browser does not support IndexedDB 2.0");
         return;
@@ -55,29 +85,33 @@ promisedTest('binaryIndex', async () => {
     equal (x.length, 2, "Should find both keys even though the second has another binary type (IndexedDB should not distinguish them)");
 });
 
-promisedTest('or query', async () => {
+promisedTest('OR-query', async () => {
     if (!supports("binarykeys")) {
         ok(true, "This browser does not support IndexedDB 2.0");
         return;
     }
+    try {
+        await db.items.bulkAdd([
+            {
+                id: new Float32Array([6.3, 10.5]),
+                data: "something"
+            },
+            {
+                id: new Uint8Array([1,2,3]),
+                data: "somethingelse"
+            }
+        ]);
+        
 
-    await db.items.bulkAdd([
-        {
-            id: new Float32Array([6.3, 10.5]),
-            data: "something"
-        },
-        {
-            id: new Uint8Array([1,2,3]),
-            data: "somethingelse"
-        }
-    ]);
-    
-
-    let a = await db.items.where('data').equals("something")
-        .or('id').equals(new Uint8Array([1,2,3]))
-        .toArray();
-    
-    equal (a.length, 2, "Should get two entries");
-    ok (a.some(x => x.data === "something"), "Should get 'something' in the result");
-    ok (a.some(x => x.data === "somethingelse"), "Should get 'somethingelse' in the result");
+        let a = await db.items.where('data').equals("something")
+            .or('id').equals(new Uint8Array([1,2,3]))
+            .toArray();
+        
+        equal (a.length, 2, "Should get two entries");
+        ok (a.some(x => x.data === "something"), "Should get 'something' in the result");
+        ok (a.some(x => x.data === "somethingelse"), "Should get 'somethingelse' in the result");
+    } finally {
+        // Cleanup. This is only needed because of a firefox bug: https://bugzilla.mozilla.org/show_bug.cgi?id=1395071
+        await db.items.clear();        
+    }
 });
