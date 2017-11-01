1d47a7105e7de3ac4413a8b9bb668483a3d74fe1
David Fahlander <david.fahlander@gmail.com>
2017-11-01 11:43:03 +0100

    WIP: Need to work on Dexie.js before continuing

--- addons/dexie-observable/src/dexie-observable.ts
+++ addons/dexie-observable/src/dexie-observable.ts
@@ -6,7 +6,26 @@ import { collectionObserve } from './collection-observe';
 import { dbObserve } from './db-observe';
 
 export default function dexieObservable (db: Dexie) {
-  // TODO: Add methods observe() on Collection and db.
   db.Collection.prototype.observe = collectionObserve;
   db.observe = dbObserve;
+
+  // TODO in dexie before continuing here:
+  // Define _createTransaction in Dexie.d.ts
+  // Implement "A simple writing hook" from https://github.com/dfahlander/Dexie.js/issues/427
+  // Implement options to deleting hook in order to not having to fetch objects prior to react on a deletion
+  
+  
+  /* Remove the following code: Don't think we need to hook into _createTransaction(). We need only to
+    add hooks on all tables (table.hook('creating'), table.hook('writing') table.hook('simple deleting'))
+  
+  let dbAsAny = db as any;
+  dbAsAny._createTransaction = Dexie.override(dbAsAny._createTransaction, _createTransaction =>{
+    return function () {
+      const trans : Dexie.Transaction = _createTransaction.apply(this, arguments);
+      const storeNames = trans.storeNames;
+      trans.on('complete', ()=>{
+
+      });
+    }
+  })*/
 }
