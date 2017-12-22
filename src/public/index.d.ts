f6d67fbf784b53245a423c76db8dd094a913342a
David Fahlander <david.fahlander@gmail.com>
2017-12-22 01:45:06 +0100

    Fixed public/index.d.ts for backward compatibility

--- src/public/index.d.ts
+++ src/public/index.d.ts
@@ -1,53 +1,49 @@
 // Type definitions for Dexie v{version}
 // Project: https://github.com/dfahlander/Dexie.js
 // Definitions by: David Fahlander <http://github.com/dfahlander>
-  
-/*export * from './types/indexable-type';
+
+export * from './types/collection';
+export * from './types/database';
+export * from './types/db-events';
+export * from './types/db-schema';
+export * from './types/dexie-constructor';
+export * from './types/dexie-dom-dependencies';
+export * from './types/dexie-event-set';
+export * from './types/dexie-event';
+export * from './types/dexie';
+export * from './types/errors';
+export * from './types/index-spec';
+export * from './types/indexable-type';
+export * from './types/promise-extended';
+export * from './types/table-hooks';
+export * from './types/table-schema';
+export * from './types/table';
+export * from './types/then-shortcut';
+export * from './types/transaction-events';
 export * from './types/transaction-mode';
-export * from './interfaces/collection';
-export * from './interfaces/db-events';
-export * from './interfaces/dexie-constructor';
-export * from './interfaces/dexie-event-set';
-export * from './interfaces/dexie-event';
-export * from './interfaces/index-spec';
-export * from './interfaces/table-hooks';
-export * from './interfaces/table-schema';
-export * from './interfaces/table';
-export * from './interfaces/transaction';
-export * from './interfaces/version';
-export * from './interfaces/where-clause';*/
+export * from './types/transaction';
+export * from './types/version';
+export * from './types/where-clause';
 
-import {Dexie as _Dexie} from './interfaces/dexie';
-import {DexieOptions} from './interfaces/dexie-constructor';
-import { DexieConstructor } from './interfaces/dexie-constructor';
-import { PromiseExtended } from './interfaces/promise-extended';
-import {Version as _Version} from './interfaces/version';
-import {Transaction as _Transaction} from './interfaces/transaction';
-import { DexieEvent as _DexieEvent } from './interfaces/dexie-event';
-import { DexieVersionChangeEvent as _DexieVersionChangeEvent } from './interfaces/db-events';
-import { Table as _Table} from './interfaces/table';
-import { IDBValidKey } from './interfaces/indexed-db';
+import { DexieConstructor} from './types/dexie-constructor';
+import { PromiseExtended } from './types/promise-extended';
+import { Version as IVersion} from './types/version';
+import { Transaction as ITransaction} from './types/transaction';
+import { Table as ITable} from './types/table';
+import { IndexableType } from './types/indexable-type';
+import { WhereClause as IWhereClause} from './types/where-clause';
+import { Collection as ICollection} from './types/collection';
 
-/*export declare class Dexie {
-  //constructor (databaseName: string, options?: DexieOptions)
-}*/
-
-export declare module Dexie {
-  interface Promise extends PromiseExtended {}
-  interface Version extends _Version {}
-  interface Transaction extends _Transaction {}
-  interface DexieEvent extends _DexieEvent {}
-  interface DexieVersionChangeEvent extends _DexieVersionChangeEvent {}
-  interface Table<T,TKey extends IDBValidKey> extends _Table<T,TKey> {}
+// For backard compatibility:
+export declare namespace Dexie {
+  type Promise<T> = PromiseExtended<T> // Because many samples have been Dexie.Promise.
+  interface Version extends IVersion {} // Because addons may want to extend Dexie.Version for old dexies as well.
+  interface Transaction  extends ITransaction {} // Because app code may declare it.
+  interface Table<T,TKey extends IndexableType> extends ITable<T,TKey> {} // Because all samples have been Dexie.Table<...>
+  interface WhereClause<T,TKey extends IndexableType> extends IWhereClause<T, TKey> {}
+  interface Collection<T,TKey extends IndexableType> extends ICollection<T, TKey> {} // Because app-code may declare it.
 }
 
-export {
-  IDBValidKey,
-  _Table as Table,
-  _Dexie as Dexie
-};
-
-export declare var Dexie: DexieConstructor;
-//export interface Dexie extends _Dexie {}
+declare var Dexie: DexieConstructor;
 
 export default Dexie;
