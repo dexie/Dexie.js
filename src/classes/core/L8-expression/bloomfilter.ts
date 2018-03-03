a4795c02782d669fc98b9a44f4e7865cdfd87350
dfahlander <david.fahlander@gmail.com>
2018-03-04 00:17:02 +0100

    Bloomfilter corrected and extended Corrected: Using correct shift operator Corrected: Using modulus bucketSize Extended: and() and or() methods.

--- src/classes/core/L8-expression/bloomfilter.ts
+++ src/classes/core/L8-expression/bloomfilter.ts
@@ -1,39 +1,68 @@
+import { assert } from '../../../functions/utils';
 
 export interface BloomFilter<Key=any> {
+  readonly bitmap: Uint32Array;
   addKeys(keys: Key[]): void;
-  intersection(): BloomFilter<Key>;
+  and(other: BloomFilter): BloomFilter;
+  or(other: BloomFilter): BloomFilter;
+  has(key): boolean;
   filter(keys: Key[]): Key[];
 }
 
-export function BloomFilter(
+export function BloomFilter (
   hash: (a) => number,
-  size: number,
-  intersection?: Uint32Array)
-  : BloomFilter
+  numBuckets: number) : BloomFilter
 {
-  const bitmap = new Uint32Array(Math.ceil(size / 32));
+  const bitmap = new Uint32Array(Math.ceil(numBuckets / 32));
 
   return {
+    bitmap,
     addKeys (keys) {
       for (let i=0,l=keys.length; i<l; ++i) {
-        const digest = hash(keys[i]);
-        const pos = digest << 5;
+        const digest = hash(keys[i]) % numBuckets;
+        const pos = digest >> 5;
         const dword = 1 << (digest & 31);
-        if (!intersection || (intersection[pos] & dword)) {
-          bitmap[pos] |= dword;
-        }
+        bitmap[pos] |= dword;
       }
     },
 
-    intersection() {
-      return BloomFilter(hash, size, bitmap);
+    and (other) {
+      const resultFilter = BloomFilter(hash, numBuckets);
+      const result = resultFilter.bitmap;
+      const otherBitmap = other.bitmap;
+      assert(otherBitmap.length === bitmap.length);
+      for (let i=0, l=bitmap.length; i<l; ++i) {
+        result[i] = bitmap[i] & otherBitmap[i];
+      }
+      return resultFilter;
+    },
+
+    or (other) {
+      const resultFilter = BloomFilter(hash, numBuckets);
+      const result = resultFilter.bitmap;
+      const otherBitmap = other.bitmap;
+      assert(otherBitmap.length === bitmap.length);
+      for (let i=0, l=bitmap.length; i<l; ++i) {
+        result[i] = bitmap[i] | otherBitmap[i];
+      }
+      return resultFilter;
+    },
+
+    has(key) {
+      const digest = hash(key) % numBuckets;
+      return !!(bitmap[digest >> 5] & (1 << (digest & 31)));
     },
     
     filter(keys: any[]) {
-      return keys.filter(key => {
-        const digest = hash(keys);
-        return bitmap[digest << 5] & (1 << (digest & 31));
-      });
+      const result = [];
+      for (let i=0,l=keys.length; i<l; ++i) {
+        const key = keys[i];
+        const digest = hash(key) % numBuckets;
+        if (bitmap[digest >> 5] & (1 << (digest & 31))) {
+          result.push(key);
+        }
+      }
+      return result;
     }
   };
 }
