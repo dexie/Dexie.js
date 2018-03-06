229a959e9d741b8f7d15efbb27db687d519345e4
dfahlander <david.fahlander@gmail.com>
2018-03-06 15:15:44 +0100

    Bugfix cursor iteration

--- src/classes/core/L1-dbcore/dbcore-indexeddb.ts
+++ src/classes/core/L1-dbcore/dbcore-indexeddb.ts
@@ -3,6 +3,8 @@ import { IDBObjectStore, IDBRequest, IDBCursor, IDBTransaction, IDBKeyRange } fr
 import { getCountAndGetAllEmulation } from './utils/index';
 import { isArray } from '../../../functions/utils';
 
+const cmp = indexedDB.cmp.bind(indexedDB);
+
 // Move these to separate module(s)
 export function eventRejectHandler(reject) {
   return event => {
@@ -115,14 +117,16 @@ function openCursor ({trans, table, index, want, limit, range, reverse, unique}:
         return;
       }
       let veryFirst = true;
-      const _cursorContinue = cursor.continue;
-      const _cursorContinuePrimaryKey = cursor.continuePrimaryKey;
-      const _cursorAdvance = cursor.advance;
-      const doThrowCursorIsStopped = ()=>{throw new Error("Cursor is stopped");}
+      const _cursorContinue = cursor.continue.bind(cursor);
+      const _cursorContinuePrimaryKey = cursor.continuePrimaryKey.bind(cursor);
+      const _cursorAdvance = cursor.advance.bind(cursor);
+      const doThrowCursorIsStopped = ()=>{throw new Error("Cursor not started");}
+      cursor.stop = doThrowCursorIsStopped;
       cursor.start = (callback, key?, primaryKey?) => {
         const iterationPromise = new Promise<void>((resolveIteration, rejectIteration) =>{
+          req.onerror = eventRejectHandler(rejectIteration);
           cursor.stop = value => {
-            cursor.continue = cursor.continuePrimaryKey = cursor.advance = doThrowCursorIsStopped;
+            cursor.stop = cursor.continue = cursor.continuePrimaryKey = cursor.advance = doThrowCursorIsStopped;
             cursor.fail = req.onerror = rejectIteration;
             resolveIteration(value);
           }
@@ -142,20 +146,30 @@ function openCursor ({trans, table, index, want, limit, range, reverse, unique}:
           }
         };
         // Call it once for the first entry, so it can call cursor.continue()
+        const isAtGivenKeys = (
+          key == null ||
+          (cmp(key, cursor.key) === 0 && 
+            (primaryKey == null || cmp(primaryKey, cursor.primaryKey) === 0))
+        );
         if (veryFirst) {
           veryFirst = false;
-          if (key == null || indexedDB.cmp(key, cursor.key) === 0) {
-            if (primaryKey == null || indexedDB.cmp(primaryKey, cursor.primaryKey) === 0) {
-              // Either user did not specify key/primaryKey as optional args to cursor.start(),
-              // so we should provide the first entry it landed on already. OR, user provided key /primaryKey
-              // but the cursor happens to be exactly on that key/primaryKey! Then we must callback!
-              guardedCallback();
-            }
+          if (isAtGivenKeys) {
+            // Either user did not specify key/primaryKey as optional args to cursor.start(),
+            // so we should provide the first entry it landed on already. OR, user provided key /primaryKey
+            // but the cursor happens to be exactly on that key/primaryKey! Then we must callback!
+            guardedCallback();
+            return iterationPromise;
           }
         }
+        // This is the second time cursor.start() is called OR, it's the first, but we're not yet at given keys.
+        // We did not call guardedCallback() so 
+        // we must do the call to cursor.continue() ourselves we should ever see anything happen.
+        // Depending on whether a key/primaryKey is given, call the proper continue method:
         if (primaryKey != null) {
+          // Both key and primaryKey was given.
           cursor.continuePrimaryKey(key, primaryKey);
         } else {
+          // key may be given. Or not.
           cursor.continue(key || null);
         }
         return iterationPromise;
@@ -295,7 +309,7 @@ export function createDBCore (db: IDBDatabase, indexedDB: IDBFactory, schema: Sc
     },
 
     //comparer: () => indexedDB.cmp.bind(indexedDB),
-    cmp: indexedDB.cmp.bind(indexedDB),
+    cmp,
 
     rangeIncludes,
 
