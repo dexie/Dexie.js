03b054dec1f016da1961db2ec324ef4642150c6d
Eugene Timokhov <timocov@gmail.com>
2018-03-09 02:15:35 +0300

    Updated dts-bundle-generator and removed appending default export from dts

--- tools/append.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const args = process.argv.slice(2);
-const [file, appentionArg] = args.filter(arg => arg[0] !== '-');
-const isText = args.some(arg => arg === '--text');
-
-const appention = isText ? appentionArg : fs.readFileSync(appentionArg);
-
-fs.appendFileSync(file, appention);
