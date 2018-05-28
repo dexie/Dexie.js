3ca644c8098f6641d958dd0897ab509e38d9b5ef
Aral Balkan <aral@ind.ie>
2018-05-28 22:16:54 +0100

    Test runner now runs all tests
    (There are failing tests and some tests are timing out but all test files are run.)

---
+++ test/run-node-test.js
@@ -0,0 +1,52 @@
+// This script is meant to be run by run-node-tests.js, once for each test
+// file that should be executed.
+const path = require('path')
+const fs = require('fs')
+
+const testFile = process.argv[2]
+
+function info (message) { console.log (message) }
+
+info(`Loading tests: ${testFile}`)
+
+let tests = fs.readFileSync(path.join(__dirname, testFile), 'utf-8')
+tests = tests.replace(/^\uFEFF/, '')                        // Strip BOM
+tests = tests.replace(/^import .*?$/gm, '')                 // Strip import statements
+tests = tests.replace(/module[/s]*?\(/gm, 'qUnitModule(')   // Transform module( => qUnitModule(
+
+info('Patching IndexedDB and the tests for the Node environment.')
+
+var QUnit = require('qunitjs')
+var qunitTap = require('qunit-tap');
+qunitTap(QUnit, function () { console.log.apply(console, arguments) })
+QUnit.config.autorun = false
+QUnit.config.testTimeout = 3000
+
+const Dexie = require('../dist/dexie.js')
+
+// Add IndexedDBShim
+const setGlobalVars = require('indexeddbshim')
+const shim = {}
+setGlobalVars(shim, {checkOrigin: false})
+const { indexedDB, IDBKeyRange } = shim
+
+Dexie.dependencies.indexedDB = indexedDB
+Dexie.dependencies.IDBKeyRange = IDBKeyRange
+
+const qUnitModule = require('qunitjs').module                                                           // eslint-disable-line no-unused-vars
+const {stop, start, asyncTest, test, equal, ok} = require('qunitjs')                                          // eslint-disable-line no-unused-vars
+const {promisedTest, spawnedTest, supports, resetDatabase} = require('./dexie-unittest-utils-node.js')  // eslint-disable-line no-unused-vars
+
+// Monkey patch the test of IndexedDB and promises (node has promises and we
+// just included the IndexedDBShim)
+async function isIdbAndPromiseCompatible () { return Promise.resolve(true) }
+
+// Since Node.js doesn’t have native Blob or FileReader support, use polyfills.
+const Blob = require('w3c-blob')
+const FileReader = require('filereader')
+
+info('Running tests…')
+
+eval(tests) // eslint-disable-line no-eval
+
+QUnit.load()
--- test/run-node-tests.js
+++ test/run-node-tests.js
@@ -1,106 +1,93 @@
 const path = require('path')
 const fs = require('fs')
+const util = require('util');
+const glob = util.promisify(require('glob'));
+const childProcess = require('child-process-es6-promise')
 
 function info (message) { console.log (message) }
 
-info('Preparing Dexie tests for Node.js.')
+(async function () {
+  info('Preparing Dexie tests for Node.js.')
 
-//
-// Extract the list of tests from the tests-all.js file.
-//
-const tranformations = [
-  {find: /^\uFEFF/, replace: ''},                 // Remove BOM symbol
-  {find: /import "\.\/(.*?)";/g, replace: "$1"},  // Remove the import "…" shell
-  {find: /\/\/[\s\S]*?$/, replace: ''},           // Remove commented-out lines
-]
-const testsAllJS = fs.readFileSync('./tests-all.js', 'utf-8')
+  //
+  // Extract the list of tests from the tests-all.js file.
+  //
+  const tranformations = [
+    {find: /^\uFEFF/, replace: ''},                 // Remove BOM symbol
+    {find: /import "\.\/(.*?)";/g, replace: "$1"},  // Remove the import "…" shell
+    {find: /\/\/[\s\S]*?$/, replace: ''},           // Remove commented-out lines
+  ]
+  const testsAllJS = fs.readFileSync('./tests-all.js', 'utf-8')
 
-const testFilesString = tranformations.reduce((document, transformation) => {
-  return document.replace(transformation.find, transformation.replace)
-}, /* start with */ testsAllJS)
+  const testFilesString = tranformations.reduce((document, transformation) => {
+    return document.replace(transformation.find, transformation.replace)
+  }, /* start with */ testsAllJS)
 
-// Get the list of files as an array, filtering out any empty entries.
-// const testFiles = testFilesString.split('\n').filter((i) => { return i !== '' })
+  // Get the list of files as an array, filtering out any empty entries.
+  const testFiles = testFilesString.split('\n').filter((i) => { return i !== '' })
 
-// Debug: start by testing one file at a time
-let testFiles = ['./tests-table.js']
+  info('Preparing unit test utilities for use in Node.')
 
-info(`Loading ${testFiles.length} tests.`)
+  let dexieUnittestUtilsJS = fs.readFileSync('./dexie-unittest-utils.js', 'utf-8').replace(/^\uFEFF/, '').replace(/^import .*?$/gm, '')
 
-const testBundle = testFiles.reduce((testBundle, testFile) => {
-  let tests = fs.readFileSync(path.join(__dirname, testFile), 'utf-8')
+  // Find exported method names and prepare the module.exports statement for the
+  // Node.js version of dexie-unit-test-utils.js
+  const exportedMethodNameRegex = /export function[\s]*?([\S]*?)([\s]*?)\(/gm
+  const matches = []
+  let match = exportedMethodNameRegex.exec(dexieUnittestUtilsJS)
+  while (match !== null) {
+    matches.push(match[1])
+    match = exportedMethodNameRegex.exec(dexieUnittestUtilsJS)
+  }
+  const moduleExportsStatement = (matches.reduce((moduleExportsStatement, currentMethodName) => {
+    return `${moduleExportsStatement} ${currentMethodName}, `
+  }, /* start with */ 'module.exports = {')).replace(/, $/, ' }')
 
-  tests = tests.replace(/^\uFEFF/, '')                        // Strip BOM
-  tests = tests.replace(/^import .*?$/gm, '')                 // Strip import statements
-  tests = tests.replace(/module[/s]*?\(/gm, 'qUnitModule(')   // Transform module( => qUnitModule(
+  // Transform: export function => function
+  dexieUnittestUtilsJS = dexieUnittestUtilsJS.replace(/export function/gm, 'function')
 
-  return testBundle += tests
-}, /* start with */ '')
+  // The require statements
+  const requireStatements = "const Dexie = require('../dist/dexie.js')\nconst {ok, start, test, config} = require('qunitjs')"
 
-info('Preparing unit test utilities for use in Node.')
+  // Add the module.exports statement to the end of the file
+  const dexieUnittestUtilsNodeJS = `${requireStatements}\n${dexieUnittestUtilsJS}\n${moduleExportsStatement}\n`
 
-let dexieUnittestUtilsJS = fs.readFileSync('./dexie-unittest-utils.js', 'utf-8').replace(/^\uFEFF/, '').replace(/^import .*?$/gm, '')
+  // Save the new utilities file temporarily so we can require it later.
+  fs.writeFileSync('./dexie-unittest-utils-node.js', dexieUnittestUtilsNodeJS)
 
-// Find exported method names and prepare the module.exports statement for the
-// Node.js version of dexie-unit-test-utils.js
-const exportedMethodNameRegex = /export function[\s]*?([\S]*?)([\s]*?)\(/gm
-const matches = []
-let match = exportedMethodNameRegex.exec(dexieUnittestUtilsJS)
-while (match !== null) {
-  matches.push(match[1])
-  match = exportedMethodNameRegex.exec(dexieUnittestUtilsJS)
-}
-const moduleExportsStatement = (matches.reduce((moduleExportsStatement, currentMethodName) => {
-  return `${moduleExportsStatement} ${currentMethodName}, `
-}, /* start with */ 'module.exports = {')).replace(/, $/, ' }')
+  // Run the tests, each one as a separate process so that failures in a specific
+  // test do not prevent later tests from running.
+  async function runTests () {
+    for (let i = 0; i < testFiles.length; i++) {
+      try {
+        const testFile = testFiles[i]
+        const childPromise = childProcess.fork('./run-node-test', [testFile])
+        const child = childPromise.child
+        // Individual test files are given 10 seconds to complete or terminated.
+        // (One of the test files is getting stuck at the moment so this allows the
+        // rest to run.)
+        const timeout = setTimeout(() => {
+          info(`*** Killing test script: ${testFile} ***`)
+          child.kill()
+        }, 10000);
+        await childPromise
+        clearTimeout(timeout)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
 
-// Transform: export function => function
-dexieUnittestUtilsJS = dexieUnittestUtilsJS.replace(/export function/gm, 'function')
+  await runTests()
 
-// The require statements
-const requireStatements = "const Dexie = require('../dist/dexie.js')\nconst {ok, start, test, config} = require('qunitjs')"
+  info('All tests run.')
 
-// Add the module.exports statement to the end of the file
-const dexieUnittestUtilsNodeJS = `${requireStatements}\n${dexieUnittestUtilsJS}\n${moduleExportsStatement}\n`
+  // Clean up
+  fs.unlinkSync('./dexie-unittest-utils-node.js')
+  const databases = await glob('D_*')
+  for (let i = 0; i < databases.length; i++) {
+    fs.unlinkSync(databases[i])
+  }
+  fs.unlinkSync('./__sysdb__.sqlite')
 
-// Save the new utilities file temporarily so we can require it later.
-fs.writeFileSync('./dexie-unittest-utils-node.js', dexieUnittestUtilsNodeJS)
-
-info('Patching IndexedDB and the tests for the Node environment.')
-
-var QUnit = require('qunitjs')
-var qunitTap = require('qunit-tap');
-qunitTap(QUnit, function () { console.log.apply(console, arguments) })
-QUnit.config.autorun = false
-
-const Dexie = require('../dist/dexie.js')
-
-// Add IndexedDBShim
-const setGlobalVars = require('indexeddbshim')
-const shim = {}
-setGlobalVars(shim, {checkOrigin: false})
-const { indexedDB, IDBKeyRange } = shim
-
-Dexie.dependencies.indexedDB = indexedDB
-Dexie.dependencies.IDBKeyRange = IDBKeyRange
-
-const qUnitModule = require('qunitjs').module                                                           // eslint-disable-line no-unused-vars
-const {stop, start, asyncTest, test, equal, ok} = require('qunitjs')                                          // eslint-disable-line no-unused-vars
-const {promisedTest, spawnedTest, supports, resetDatabase} = require('./dexie-unittest-utils-node.js')  // eslint-disable-line no-unused-vars
-
-// Monkey patch the test of IndexedDB and promises (node has promises and we
-// just included the IndexedDBShim)
-async function isIdbAndPromiseCompatible () { return Promise.resolve(true) }
-
-// Since Node.js doesn’t have native Blob or FileReader support, use polyfills.
-const Blob = require('w3c-blob')
-const FileReader = require('filereader')
-
-info('Running tests…')
-
-eval(testBundle) // eslint-disable-line no-eval
-
-QUnit.load()
-
-// Clean up
-fs.unlinkSync('./dexie-unittest-utils-node.js')
+})()
