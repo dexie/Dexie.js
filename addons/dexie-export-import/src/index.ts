28c7d784ca7cd5c03429fa1ad576ebc3753336cd
dfahlander <david.fahlander@gmail.com>
2018-10-23 19:17:40 +0200

    Changing the addon to a global addon.

--- addons/dexie-export-import/src/index.ts
+++ addons/dexie-export-import/src/index.ts
@@ -1,11 +1,12 @@
-import { ExportOptions } from './export';
-import { ImportOptions } from './import';
+import Dexie from 'dexie';
+import { ExportOptions, exportDB } from './export';
+import { importDB, ImportOptions, importInto } from './import';
 
-export {exportDB, ExportOptions} from './export';
-export {importDB, importInto, ImportOptions} from './import';
+export { exportDB, ExportOptions};
+export { importDB, importInto, ImportOptions};
 
 //
-// Extend Dexie interface
+// Extend Dexie interface (typescript-wise)
 //
 declare module 'dexie' {
   // Extend methods on db
@@ -15,4 +16,17 @@ declare module 'dexie' {
   }
 }
 
-export * from './addon';
+//
+// Extend Dexie interface (runtime wise)
+//
+
+Dexie.prototype.export = function (this: Dexie, options?: ExportOptions) {
+  return exportDB(this, options);
+}
+Dexie.prototype.import = function (this: Dexie, blob: Blob, options?: ImportOptions) {
+  return importInto(this, blob, options);
+}
+
+export default ()=>{
+  throw new Error("This addon extends Dexie.prototype globally and does not have be included in Dexie constructor's addons options.")
+};
