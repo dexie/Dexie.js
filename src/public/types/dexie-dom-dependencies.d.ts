eb20cf8f7a8260dd8c28802fb3906050b394e142
Brent McSharry <brentm@adhb.govt.nz>
2019-03-31 18:27:20 +1300

    Revert "avoid 2x call setDatabaseEnumerator"
    This reverts commit a70d043a03a1eb39fa66c7a2e47a3e47ced80be2.

--- src/public/types/dexie-dom-dependencies.d.ts
+++ src/public/types/dexie-dom-dependencies.d.ts
@@ -1,7 +1,4 @@
-import { IDatabaseEnumerator } from "../../helpers/database-enumerator";
-
 export interface DexieDOMDependencies {
   indexedDB: IDBFactory;
   IDBKeyRange: typeof IDBKeyRange;
-  databaseEnumerator: IDatabaseEnumerator;
 }
