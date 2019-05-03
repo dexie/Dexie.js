1376def1a32200c74cae3e6bee5651eabb03daee
dfahlander <david.fahlander@gmail.com>
2019-05-03 18:45:43 +0200

    Small changes

--- samples/react-suspense/src/App.js
+++ samples/react-suspense/src/App.js
@@ -1,5 +1,5 @@
 import React, { useCallback, useState } from 'react';
-import { Observe } from 'fiberlib-react';
+import { Observe } from 'react-observe';
 import './App.css';
 
 import db from './db';
@@ -8,8 +8,8 @@ import { TodoList } from './TodoList';
 
 export const App = () => {
   // State
-  const [showCompleted, setShowCompleted] = useState(false);
   const [error, setError] = useState(null);
+  const [showCompletedTodos, setShowCompletedTodos] = useState(false);
 
   // View
   return <Observe>
@@ -18,33 +18,36 @@ export const App = () => {
         <h2>React Suspense + Dexie Todo Example</h2>
       </div>
 
-      {error && <div className="error">
-        <p className="error">An error occurred: {error}</p>
-        <button onClick={()=>setError(null)}>Got it!</button>
-      </div>}
-
       <AddTodo handleAddTodo={title =>
         db.todos.add({title, done: 0}).catch(setError)} />
 
       <div>
         <input
           type="checkbox"
-          checked={showCompleted}
-          onChange={ev => setShowCompleted(ev.target.checked)}
+          checked={showCompletedTodos}
+          onChange={ev => setShowCompletedTodos(ev.target.checked)}
         />
         <span>Show completed</span>
       </div>
       
-      <TodoList todos={
-        showCompleted ?
-          db.todos.orderBy('done') :
-          db.todos.where({done: 0})
+      <TodoList
+        todos={showCompletedTodos ?
+          db.todos.orderBy('done') : // List undone first, then by date/time
+          db.todos.where({done: 0})  // List only undone
         }
+        
         handleToggleTodo={(id, done) =>
           db.todos.update(id, {done}).catch(setError)}
+
         handleDeleteTodo={id =>
           db.todos.delete(id).catch(setError)}
+          
       />
+
+      {error && <div className="error">
+        <p>An error has occurred: {error}</p>
+        <button onClick={()=>setError(null)}>Got it!</button>
+      </div>}
 
     </div>
   </Observe>
