85783bff09dc11760b5d86bcd2ac4b53fc3bc206
Alex Moon <alex.jared.moon@gmail.com>
2020-04-16 07:40:11 +0000

    fix: add missing properties on observable change

--- addons/Dexie.Observable/api.d.ts
+++ addons/Dexie.Observable/api.d.ts
@@ -1,44 +1,47 @@
 /**
  * API for Dexie.Observable.
- * 
+ *
  * Contains interfaces used by dexie-observable.
- * 
+ *
  * By separating module 'dexie-observable' from 'dexie-observable/api' we
  * distinguish that:
- * 
+ *
  *   import {...} from 'dexie-observable/api' is only for getting access to its
  *                                            interfaces and has no side-effects.
  *                                            Typescript-only import.
- * 
+ *
  *   import 'dexie-observable' is only for side effects - to extend Dexie with
  *                             functionality of dexie-observable.
  *                             Javascript / Typescript import.
- * 
+ *
  */
 export const enum DatabaseChangeType {
-    Create = 1,
-    Update = 2,
-    Delete = 3
+  Create = 1,
+  Update = 2,
+  Delete = 3,
 }
 
 export interface ICreateChange {
-    type: DatabaseChangeType.Create,
-    table: string;
-    key: any;
-    obj: any;
+  type: DatabaseChangeType.Create;
+  table: string;
+  key: any;
+  obj: any;
 }
 
 export interface IUpdateChange {
-    type: DatabaseChangeType.Update;
-    table: string;
-    key: any;
-    mods: {[keyPath: string]:any | undefined};
+  type: DatabaseChangeType.Update;
+  table: string;
+  key: any;
+  mods: { [keyPath: string]: any | undefined };
+  obj: any;
+  oldObj: any;
 }
 
 export interface IDeleteChange {
-    type: DatabaseChangeType.Delete;
-    table: string;
-    key: any;
+  type: DatabaseChangeType.Delete;
+  table: string;
+  key: any;
+  oldObj: any;
 }
 
-export type IDatabaseChange = ICreateChange | IUpdateChange | IDeleteChange; 
+export type IDatabaseChange = ICreateChange | IUpdateChange | IDeleteChange;
