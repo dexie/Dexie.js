3f364f3ea85e063026b20b6e0e7b95191e82331e
dfahlander <david.fahlander@gmail.com>
2020-09-04 13:49:56 +0200

    Typings of Observable / Syncable for Dexie 3.0

--- addons/Dexie.Observable/src/Dexie.Observable.d.ts
+++ addons/Dexie.Observable/src/Dexie.Observable.d.ts
@@ -1,9 +1,53 @@
-// Type definitions for dexie-observable v{version}
+// Type definitions for dexie-observable v1.0.0-beta.8
 // Project: https://github.com/dfahlander/Dexie.js/tree/master/addons/Dexie.Observable
 // Definitions by: David Fahlander <http://github.com/dfahlander>
 
-import Dexie from 'dexie';
+import Dexie, { DexieEventSet } from 'dexie';
 import { IDatabaseChange } from '../api';
+
+export interface SyncNodeConstructor {
+    new() : SyncNode;
+}
+
+//
+// Interfaces of Dexie.Observable
+//            
+
+
+/**
+ * A SyncNode represents a local database instance that subscribes
+ * to changes made on the database.
+ * SyncNodes are stored in the _syncNodes table.
+ * 
+ * Dexie.Syncable extends this interface and allows 'remote' nodes to be stored
+ * as well.
+ */
+export interface SyncNode {
+    id?: number,
+    myRevision: number,
+    type: 'local' | 'remote',
+    lastHeartBeat: number,
+    deleteTimeStamp: number, // In case lastHeartBeat is too old, a value of now + HIBERNATE_GRACE_PERIOD will be set here. If reached before node wakes up, node will be deleted.
+    isMaster: number // 1 if true. Not using Boolean because it's not possible to index Booleans.
+}
+
+export interface ObservableEventSet extends DexieEventSet {
+    (eventName: 'latestRevisionIncremented', subscriber: (dbName: string, latestRevision: number) => void): void;
+    (eventName: 'suicideNurseCall', subscriber: (dbName: string, nodeID: number) => void): void;
+    (eventName: 'intercomm', subscriber: (dbName: string) => void): void;
+    (eventName: 'beforeunload', subscriber: () => void): void;
+}
+
+// Object received by on('message') after sendMessage() or broadcastMessage()
+interface MessageEvent {
+    id: number;
+    type: string;
+    message: any;
+    destinationNode: number;
+    wantReply?: boolean;
+    resolve(result: any): void;
+    reject(error: any): void;
+}
 
 //
 // Extend Dexie interface
@@ -14,7 +58,7 @@ declare module 'dexie' {
         // Placeholder where to access the SyncNode class constructor.
         // (makes it valid to do new db.observable.SyncNode())
         observable: {
-            SyncNode: Dexie.Observable.SyncNodeConstructor;
+            SyncNode: SyncNodeConstructor;
             sendMessage(
                 type: string, // Don't use 'response' as it is used internally by the framework
                 message: any, // anything that can be saved by IndexedDB
@@ -31,34 +75,34 @@ declare module 'dexie' {
             ): void;
         }
 
-        readonly _localSyncNode: Dexie.Observable.SyncNode;
+        readonly _localSyncNode: SyncNode;
 
         _changes: Dexie.Table<IDatabaseChange & {rev: number}, number>;
-        _syncNodes: Dexie.Table<Dexie.Observable.SyncNode, number>;
+        _syncNodes: Dexie.Table<SyncNode, number>;
         _intercomm: Dexie.Table<any, number>;
     }
 
-    module Dexie {
-        // Extended events db.on('changes', subscriber), ...
-        interface DbEvents {
-            (eventName: 'changes', subscriber: (changes: IDatabaseChange[], partial: boolean)=>void): void;
-            (eventName: 'cleanup', subscriber: ()=>any): void;
-            (eventName: 'message', subscriber: (msg: Dexie.Observable.MessageEvent)=>any): void;
-        }
+    // Extended events db.on('changes', subscriber), ...
+    interface DbEvents {
+        (eventName: 'changes', subscriber: (changes: IDatabaseChange[], partial: boolean)=>void): void;
+        (eventName: 'cleanup', subscriber: ()=>any): void;
+        (eventName: 'message', subscriber: (msg: MessageEvent)=>any): void;
+    }
 
-        // Extended IndexSpec with uuid boolean for primary key.
-        interface IndexSpec {
-            uuid: boolean;
-        }
+    // Extended IndexSpec with uuid boolean for primary key.
+    interface IndexSpec {
+        uuid: boolean;
+    }
 
-        var Observable: {
+    interface DexieConstructor {
+        Observable: {
             (db: Dexie) : void;
 
             //
             //
             //
             createUUID: () => string;
-            on: Observable.ObservableEventSet;
+            on: ObservableEventSet;
             localStorageImpl: {
                 setItem(key: string, value: string): void,
                 getItem(key: string): string,
@@ -67,53 +111,6 @@ declare module 'dexie' {
             _onStorage: (event: StorageEvent) => void;
         }
 
-        //
-        // Define Dexie.Observable
-        //
-        module Observable {
-            //
-            // Interfaces of Dexie.Observable
-            //            
-
-            interface SyncNodeConstructor {
-                new() : SyncNode;
-            }
-
-            /**
-             * A SyncNode represents a local database instance that subscribes
-             * to changes made on the database.
-             * SyncNodes are stored in the _syncNodes table.
-             * 
-             * Dexie.Syncable extends this interface and allows 'remote' nodes to be stored
-             * as well.
-             */
-            interface SyncNode {
-                id?: number,
-                myRevision: number,
-                type: 'local' | 'remote',
-                lastHeartBeat: number,
-                deleteTimeStamp: number, // In case lastHeartBeat is too old, a value of now + HIBERNATE_GRACE_PERIOD will be set here. If reached before node wakes up, node will be deleted.
-                isMaster: number // 1 if true. Not using Boolean because it's not possible to index Booleans.
-            }
-
-            interface ObservableEventSet extends Dexie.DexieEventSet {
-                (eventName: 'latestRevisionIncremented', subscriber: (dbName: string, latestRevision: number) => void): void;
-                (eventName: 'suicideNurseCall', subscriber: (dbName: string, nodeID: number) => void): void;
-                (eventName: 'intercomm', subscriber: (dbName: string) => void): void;
-                (eventName: 'beforeunload', subscriber: () => void): void;
-            }
-
-            // Object received by on('message') after sendMessage() or broadcastMessage()
-            interface MessageEvent {
-                id: number;
-                type: string;
-                message: any;
-                destinationNode: number;
-                wantReply?: boolean;
-                resolve(result: any): void;
-                reject(error: any): void;
-            }
-        }
     }
 }
 
