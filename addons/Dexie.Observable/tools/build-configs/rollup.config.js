4dac8989768ac9e41df72abc87b59e995e3c98aa
dfahlander <david.fahlander@gmail.com>
2020-09-08 22:40:13 +0200

    Removed dead code

--- addons/Dexie.Observable/tools/build-configs/rollup.config.js
+++ addons/Dexie.Observable/tools/build-configs/rollup.config.js
@@ -9,18 +9,14 @@ export default {
   output: [{
     file: 'dist/dexie-observable.js',
     format: 'umd',
-    banner: readFileSync(path.resolve(__dirname, 'banner.txt'))+""
-        .replace(/{version}/g, version)
-        .replace(/{date}/g, new Date().toDateString()),
+    banner: readFileSync(path.resolve(__dirname, 'banner.txt'),
     globals: {dexie: "Dexie"},
     name: 'Dexie.Observable',
     sourcemap: true
   },{
     file: 'dist/dexie-observable.es.js',
     format: 'es',
-    banner: readFileSync(path.resolve(__dirname, 'banner.txt'))+""
-        .replace(/{version}/g, version)
-        .replace(/{date}/g, new Date().toDateString()),
+    banner: readFileSync(path.resolve(__dirname, 'banner.txt'),
     globals: {dexie: "Dexie"},
     name: 'Dexie.Observable',
     sourcemap: true
