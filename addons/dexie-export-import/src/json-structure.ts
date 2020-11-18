999101a4e8d117385026dc1065e52f6b560833af
David Fahlander <david.fahlander@gmail.com>
2020-11-18 20:20:11 +0100

    dexie-export-import: Peek import file (Solution to #1163?) (#1166)
    * Solution to #1163 ?
    
    * Don't validate the headers.
    Also, export a new interface representing the headers / meta data of the file - returned by peakImportFile().

--- addons/dexie-export-import/src/json-structure.ts
+++ addons/dexie-export-import/src/json-structure.ts
@@ -1,6 +1,21 @@
 export const VERSION = 1;
 
-export interface DexieExportJsonStructure {
+/** Same as DexieExportJsonStructure but without the data.data array */
+export interface DexieExportJsonMeta {
+  formatName: 'dexie';
+  formatVersion: typeof VERSION;
+  data: {
+    databaseName: string;
+    databaseVersion: number;
+    tables: Array<{
+      name: string;
+      schema: string;
+      rowCount: number;
+    }>;
+  }
+}
+
+export interface DexieExportJsonStructure extends DexieExportJsonMeta {
   formatName: 'dexie';
   formatVersion: typeof VERSION;
   data: {
