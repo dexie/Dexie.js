78a48f939a3b3216659995d620b7270311e06a5f
&y Haskell <andy.with.ampersand@gmail.com>
2020-11-22 18:56:36 -0500

    Add components to Vue example
    In the following commit, I will add the Dexie database class and have
    the components use it.

---
+++ samples/vue/src/database.js
@@ -0,0 +1,13 @@
+// [TODO] Add database class
+
+// forwardOrder is passed into getTodos to retrieve todos in chronological
+// order.
+export const forwardOrder = 'forward';
+
+// reverseOrder is passed into getTodos to retrieve todos in reverse
+// chronological order.
+export const reverseOrder = 'reverse';
+
+// unfinishedFirstOrder is passed into getTodos to retrieve todos such that
+// unfinished todos come before finished todos in the returned array.
+export const unfinishedFirstOrder = 'unfinished-first';
