3e97259a9cc4b5fcb66433ec329140c52de02b5a
&y Haskell <andy.with.ampersand@gmail.com>
2020-11-21 19:45:03 -0500

    Switch Vue example from using Yarn to npm
    Additionally, tidy up the code some; remove the HelloWorld component,
    add vue.config.js to serve the app on port Fibonacci

--- samples/vue/src/App.vue
+++ samples/vue/src/App.vue
@@ -1,15 +1,12 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // [TODO] Add components for to-do list
   }
 }
 </script>
