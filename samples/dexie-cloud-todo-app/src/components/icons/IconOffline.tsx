05c00892637dcb7559e90fe275e1a10bd482636c
dfahlander <david.fahlander@gmail.com>
2021-07-09 13:23:37 +0200

    dexie cloud sample app: Icons for all sync states

---
+++ samples/dexie-cloud-todo-app/src/components/icons/IconOffline.tsx
@@ -0,0 +1,29 @@
+/*!
+
+    This icon is licensed under Creative Commons license.
+
+    Attribution Text: "No cloud by Bernar  Novalyi from the Noun Project"
+
+    Make sure to give the creator credits by showing the attribution text in your app!
+    Else, buy a commercial license from The Noun Project to use the icon (https://thenounproject.com).
+    
+*/
+export const IconOffline = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    data-name="Layer 1"
+    viewBox="0 0 100 125"
+    x="0px"
+    y="0px"
+  >
+    <path d="M88.17,69.32A22,22,0,0,0,77.79,37.77a22,22,0,0,0-21.91-20.2,20.29,20.29,0,0,0-13.67,4.81l4.57,5.3a13.31,13.31,0,0,1,9.1-3.11,15,15,0,0,1,15,15c0,.06,0,.13,0,.19s0,.26,0,.39l-.1,2.75,2.65.75a15,15,0,0,1,8.79,22.1Z" />
+    <path d="M28.76,80H60.44V73H28.76a15,15,0,0,1-15-15A17,17,0,0,1,23.6,42.7l-2.79-6.42A23.91,23.91,0,0,0,6.78,58,22,22,0,0,0,28.76,80Z" />
+    <rect
+      x="45.53"
+      y="-4.58"
+      width="7"
+      height="106.75"
+      transform="translate(-20.14 48.96) rotate(-45)"
+    />
+  </svg>
+);
