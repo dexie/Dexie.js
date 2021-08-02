4c8e9e51de63fc566daa9d58b119281e65156f59
dfahlander <david.fahlander@gmail.com>
2021-08-02 12:27:17 +0200

    Bugfix: document not available in SW

--- addons/dexie-cloud/src/userIsActive.ts
+++ addons/dexie-cloud/src/userIsActive.ts
@@ -38,37 +38,39 @@ export const userDoesSomething =
       )
     : of({});
 
-//
-// Now, create a final observable and start subscribing to it in order
-// to make it emit values to userIsActive BehaviourSubject (which is the
-// most important global hot observable we have here)
-//
-// Live test: https://jsitor.com/LboCDHgbn
-//
-merge(
-  of(true), // Make sure something is always emitted from start
-  documentBecomesHidden, // so that we can eagerly emit false!
-  userDoesSomething
-)
-  .pipe(
-    // No matter event source, compute whether user is visible using visibilityState:
-    map(() => document.visibilityState === 'visible'),
-    // Make sure to emit it
-    tap((isActive) => {
-      if (userIsActive.value !== isActive) {
-        // Emit new value unless it already has that value
-        userIsActive.next(isActive);
-      }
-    }),
-    // Now, if true was emitted, make sure to set a timeout to emit false
-    // unless new user activity things happen (in that case, the timeout will be cancelled!)
-    switchMap((isActive) =>
-      isActive
-        ? of(true).pipe(
-            delay(USER_INACTIVITY_TIMEOUT),
-            tap(() => userIsActive.next(false))
-          )
-        : of(false)
-    )
+if (typeof document !== 'undefined') {
+  //
+  // Now, create a final observable and start subscribing to it in order
+  // to make it emit values to userIsActive BehaviourSubject (which is the
+  // most important global hot observable we have here)
+  //
+  // Live test: https://jsitor.com/LboCDHgbn
+  //
+  merge(
+    of(true), // Make sure something is always emitted from start
+    documentBecomesHidden, // so that we can eagerly emit false!
+    userDoesSomething
   )
-  .subscribe(() => {}); // Unless we subscribe nothing will be propagated to userIsActive observable
+    .pipe(
+      // No matter event source, compute whether user is visible using visibilityState:
+      map(() => document.visibilityState === 'visible'),
+      // Make sure to emit it
+      tap((isActive) => {
+        if (userIsActive.value !== isActive) {
+          // Emit new value unless it already has that value
+          userIsActive.next(isActive);
+        }
+      }),
+      // Now, if true was emitted, make sure to set a timeout to emit false
+      // unless new user activity things happen (in that case, the timeout will be cancelled!)
+      switchMap((isActive) =>
+        isActive
+          ? of(true).pipe(
+              delay(USER_INACTIVITY_TIMEOUT),
+              tap(() => userIsActive.next(false))
+            )
+          : of(false)
+      )
+    )
+    .subscribe(() => {}); // Unless we subscribe nothing will be propagated to userIsActive observable
+}
