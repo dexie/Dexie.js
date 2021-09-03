0f2c780caae5058a7faecc66fd32be9cfba99f8d
David Fahlander <david.fahlander@gmail.com>
2021-09-03 11:08:23 +0200

    Official event Dexie.on('storagemutated') (#1384)
    Official event Dexie.on('storagemutated').
    
    This change is a step to making dexie@3.2.0 an officially supported stable release where the observability communication channel can be official and documented.
    
    The event was previously undocumented and had the name Dexie.on('txcommitted'). The event is the channel that communicates changes on indexeddb databases to enable `liveQuery()` to detect changes across service worker, windows and tabs. Event is propagated across peers using BroadcastChannel on browsers supporting it, and "storage" event on IE and Safari.
    
    The commit does not break any officially documented Dexie API but as the rename of the "txcommitted" event to "storagemutated" and its corresponding event names in BroadcastChannel land localStorage ("x-storagemutated-1") are changed, code that would have been using those undocumented event names will need to be updated.
    
    This commit also improves observability in a rare use case when "dexie" module is imported multiple times (possibly with different versions of the package) as it also propagates on window/self event target and not just Dexie static.

--- src/globals/global-events.ts
+++ src/globals/global-events.ts
@@ -1,4 +1,19 @@
 import Events from '../helpers/Events';
 import { GlobalDexieEvents } from '../public/types/db-events';
 
-export const globalEvents = Events(null, "txcommitted") as GlobalDexieEvents;
+export const DEXIE_STORAGE_MUTATED_EVENT_NAME = 'storagemutated' as 'storagemutated';
+
+// Name of the global event fired using DOM dispatchEvent (if not in node).
+// Reason for propagating this as a DOM event is for getting reactivity across
+// multiple versions of Dexie within the same app (as long as they are
+// compatible with regards to the event data).
+// If the ObservabilitySet protocol change in a way that would not be backward
+// compatible, make sure also update the event name to a new number at the end
+// so that two Dexie instances of different versions continue to work together
+//  - maybe not able to communicate but won't fail due to unexpected data in
+// the detail property of the CustomEvent. If so, also make sure to udpate
+// docs and explain at which Dexie version the new name and format of the event
+// is being used.
+export const STORAGE_MUTATED_DOM_EVENT_NAME = 'x-storagemutated-1';
+
+export const globalEvents = Events(null, DEXIE_STORAGE_MUTATED_EVENT_NAME) as GlobalDexieEvents;
