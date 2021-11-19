362fced20294fcad7cf630ddb33565f06ac10902
dfahlander <david.fahlander@gmail.com>
2021-11-19 11:05:46 +0100

    Put back remote-sync sample as it was included... ...from unit test that otherwise failed.

---
+++ samples/remote-sync/ajax/AjaxSyncProtocol.js
@@ -0,0 +1,88 @@
+﻿/// <reference path="../../dist/dexie.js" />
+/// <reference path="../../addons/Dexie.Syncable/dist/dexie-syncable.js" />
+/// <reference path="../../addons/Dexie.Syncable/Dexie.Syncable.SyncProtocolAPI.js" />
+/// <reference path="includes/jquery-2.1.0.js" />
+
+Dexie.Syncable.registerSyncProtocol("sample_protocol", {
+
+    sync: function (context, url, options, baseRevision, syncedRevision, changes, partial, applyRemoteChanges, onChangesAccepted, onSuccess, onError) {
+        /// <param name="context" type="IPersistedContext"></param>
+        /// <param name="url" type="String"></param>
+        /// <param name="changes" type="Array" elementType="IDatabaseChange"></param>
+        /// <param name="applyRemoteChanges" value="function (changes, lastRevision, partial, clear) {}"></param>
+        /// <param name="onSuccess" value="function (continuation) {}"></param>
+        /// <param name="onError" value="function (error, again) {}"></param>
+
+        var POLL_INTERVAL = 10000; // Poll every 10th second
+
+        // In this example, the server expects the following JSON format of the request:
+        //  {
+        //      [clientIdentity: unique value representing the client identity. If omitted, server will return a new client identity in its response that we should apply in next sync call.]
+        //      baseRevision: baseRevision,
+        //      partial: partial,
+        //      changes: changes,
+        //      syncedRevision: syncedRevision
+        //  }
+        //  To keep the sample simple, we assume the server has the exact same specification of how changes are structured.
+        //  In real world, you would have to pre-process the changes array to fit the server specification.
+        //  However, this example shows how to deal with ajax to fulfil the API.
+        var request = {
+            clientIdentity: context.clientIdentity || null,
+            baseRevision: baseRevision,
+            partial: partial,
+            changes: changes,
+            syncedRevision: syncedRevision
+        };
+
+        // Send the request:
+        $.ajax(url, {
+            type: 'post',
+            contentType: 'application/json', // Make sure we set the correct content-type header as some servers expect this
+            dataType: 'json',
+            data: JSON.stringify(request),
+            error: function (xhr, textStatus) {
+                // Network down, server unreachable or other failure. Try again in POLL_INTERVAL seconds.
+                onError(textStatus, POLL_INTERVAL);
+            },
+            success: function (data) {
+                // In this example, the server response has the following format:
+                //{
+                //    success: true / false,
+                //    errorMessage: "",
+                //    changes: changes,
+                //    currentRevision: revisionOfLastChange,
+                //    needsResync: false, // Flag telling that server doesn't have given syncedRevision or of other reason wants client to resync. ATTENTION: this flag is currently ignored by Dexie.Syncable
+                //    partial: true / false, // The server sent only a part of the changes it has for us. On next resync it will send more based on the clientIdentity
+                //    [clientIdentity: unique value representing the client identity. Only provided if we did not supply a valid clientIdentity in the request.]
+                //}
+                if (!data.success) {
+                    onError (data.errorMessage, Infinity); // Infinity = Don't try again. We would continue getting this error.
+                } else {
+                    if ('clientIdentity' in data) {
+                        context.clientIdentity = data.clientIdentity;
+                        // Make sure we save the clientIdentity sent by the server before we try to resync.
+                        // If saving fails we wouldn't be able to do a partial synchronization
+                        context.save()
+                            .then(() => {
+                            // Since we got success, we also know that server accepted our changes:
+                            onChangesAccepted();
+                            // Convert the response format to the Dexie.Syncable.Remote.SyncProtocolAPI specification:
+                            applyRemoteChanges (data.changes, data.currentRevision, data.partial, data.needsResync);
+                            onSuccess({ again: POLL_INTERVAL });
+                            })
+                            .catch((e) => {
+                                // We didn't manage to save the clientIdentity stop synchronization
+                                onError(e, Infinity);
+                            });
+                    } else {
+                        // Since we got success, we also know that server accepted our changes:
+                        onChangesAccepted();
+                        // Convert the response format to the Dexie.Syncable.Remote.SyncProtocolAPI specification:
+                        applyRemoteChanges (data.changes, data.currentRevision, data.partial, data.needsResync);
+                        onSuccess({ again: POLL_INTERVAL });
+                    }
+                }
+            }
+        });
+    }
+});
---
+++ samples/remote-sync/websocket/websocketserver-shim.js
@@ -0,0 +1,153 @@
+﻿/* This shim helps you unit test both WebSocket client and a websocket server in the browser.
+   
+   This shim will implement a simple web socket server with same API as "nodejs-websocket".
+   It will also replace the browser's WebSocket class with a emulated WebSocket class that
+   will only communicate with instances of the emulated websocket server.
+
+   You can then unit test your WebSocket client and server in the same browser without relying
+   on any network card or TCP stack.
+*/
+
+(function () {
+
+    // To be able to test both server and client in a browser, emulate a WebSocketServer in the browser, and replace the
+    // WebSocket api with an emulated WebSocket that communicates with emulated WebSocketServer.
+    function EmulatedWebSocketServerFactory() {
+        this.createServer = function (connectCallback) {
+            var server = {
+                _connect: function (socket) {
+                    var conn = {
+                        _client: socket,
+                        _listeners: { "text": [], "close": [] },
+                        on: function (type, listener) {
+                            conn._listeners[type].push(listener);
+                        },
+                        _fire: function (type, args) {
+                            conn._listeners[type].forEach(function (listener) {
+                                listener.apply(null, args);
+                            });
+                        },
+
+                        _text: function (msg) {
+                            conn._fire("text", [msg]);
+                        },
+                        sendText: function (msg) {
+                            setTimeout(function () {
+                                conn._client._text(msg);
+                            }, 0);
+                        },
+                        close: function (code, reason) {
+                            setTimeout(function () {
+                                conn._client._disconnect(code, reason);
+                            }, 0);
+                        },
+                        _disconnect: function (code, reason) {
+                            conn._fire("close", [code, reason]);
+                        }
+                    };
+                    connectCallback(conn);
+                    return conn;
+                },
+                listen: function (port) {
+                    EmulatedWebSocketServerFactory.listeners[port] = this;
+                    return this;
+                }
+            }
+            return server;
+        }
+    }
+    EmulatedWebSocketServerFactory.listeners = {};
+
+    function EmulatedWebSocket(url) {
+        var uri = parseURL(url);
+        var self = this;
+        this.conn = null;
+        this.send = function (msg) {
+            if (!this.conn) throw "Not connected";
+            setTimeout(function () {
+                self.conn._text(msg);
+            }, 0);
+        }
+        this.close = function (code, reason) {
+            setTimeout(function () {
+                if (self.conn) self.conn._disconnect(code, reason);
+                self.conn = null;
+            }, 0);
+        }
+        this._text = function (msg) {
+            if (this.onmessage) {
+                this.onmessage({ target: this, data: msg });
+            }
+        }
+        this._disconnect = function (code, reason) {
+            if (this.onclose) {
+                this.onclose({code: code, reason: reason, wasClean: true });
+            }
+        }
+        setTimeout(function () {
+            try {
+                if (EmulatedWebSocketServerFactory.listeners[uri.port]) {
+                    var server = EmulatedWebSocketServerFactory.listeners[uri.port];
+                    var conn = server._connect(self);
+                    self.conn = conn;
+                    if (self.onopen) {
+                        self.onopen({ target: self });
+                    }
+                } else {
+                    throw "Could not connect";
+                }
+            } catch (e) {
+                if (self.onerror) {
+                    self.onerror({
+                        target: self,
+                        message: e.toString()
+                    });
+                }
+            }
+        }, 0);
+    }
+
+    function parseURL(url) {
+        var a = document.createElement('a');
+        a.href = url;
+        return {
+            source: url,
+            protocol: a.protocol.replace(':', ''),
+            host: a.hostname,
+            port: a.port,
+            query: a.search,
+            params: (function () {
+                var ret = {},
+                    seg = a.search.replace(/^\?/, '').split('&'),
+                    len = seg.length, i = 0, s;
+                for (; i < len; i++) {
+                    if (!seg[i]) { continue; }
+                    s = seg[i].split('=');
+                    ret[s[0]] = s[1];
+                }
+                return ret;
+            })(),
+            file: (a.pathname.match(/\/([^\/?#]+)$/i) || [, ''])[1],
+            hash: a.hash.replace('#', ''),
+            path: a.pathname.replace(/^([^\/])/, '/$1'),
+            relative: (a.href.match(/tps?:\/\/[^\/]+(.+)/) || [, ''])[1],
+            segments: a.pathname.replace(/^\//, '').split('/')
+        };
+    }
+
+    function override(origFunc, overridedFactory) {
+        return overridedFactory(origFunc);
+    }
+
+    window.require = override(window.require, function (origFunc) {
+        return function (module) {
+            if (module == "nodejs-websocket")
+                return new EmulatedWebSocketServerFactory();
+            else
+                return origFunc.apply(this, arguments);
+        }
+    });
+
+    window.WebSocket = EmulatedWebSocket;
+
+})();
