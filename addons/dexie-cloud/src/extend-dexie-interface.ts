ff3c83a0466b529fea00b4991c4f53c46c1fb025
dfahlander <david.fahlander@gmail.com>
2021-11-24 21:10:16 +0100

    db.cloud.permissions()

--- addons/dexie-cloud/src/extend-dexie-interface.ts
+++ addons/dexie-cloud/src/extend-dexie-interface.ts
@@ -1,10 +1,10 @@
-import Dexie, { Table } from 'dexie';
+import Dexie, { IndexableType, Table, TableProp } from 'dexie';
 import { DexieCloudOptions } from './DexieCloudOptions';
 import {
   DBRealm,
   DBRealmMember,
   DBRealmRole,
-  DexieCloudSchema
+  DexieCloudSchema,
 } from 'dexie-cloud-common';
 import { UserLogin } from './db/entities/UserLogin';
 import * as Rx from 'rxjs';
@@ -16,54 +16,68 @@ import { Role } from './db/entities/Role';
 import { EntityCommon } from './db/entities/EntityCommon';
 import { DXCUserInteraction } from './types/DXCUserInteraction';
 import { DXCWebSocketStatus } from './DXCWebSocketStatus';
+import { PermissionChecker } from './PermissionChecker';
 
 export interface DexieCloudSyncOptions {
   wait: boolean;
-  purpose: "push" | "pull"
+  purpose: 'push' | 'pull';
 }
 
-export type DexieCloudTable<T = any> = Table<T & EntityCommon, string>;
+export type DexieCloudTable<T = any, TKey = string> = Table<
+  T & { realmId: string; owner: string },
+  TKey,
+  'realmId' | 'owner'
+>;
+
+export interface DexieCloudAPI<TDexieSubClass extends Dexie> {
+  version: string;
+  options: DexieCloudOptions | null;
+  schema: DexieCloudSchema | null;
+  serverState: DexieCloudServerState | null;
+  currentUserId: string;
+  currentUser: Rx.BehaviorSubject<UserLogin>;
+  webSocketStatus: Rx.BehaviorSubject<DXCWebSocketStatus>;
+  syncState: Rx.BehaviorSubject<SyncState>;
+  persistedSyncState: Rx.BehaviorSubject<PersistedSyncState | undefined>;
+  userInteraction: Rx.BehaviorSubject<DXCUserInteraction | undefined>;
+  //loginState: Rx.BehaviorSubject<LoginState>;
+  usingServiceWorker?: boolean;
+
+  /** Login using Dexie Cloud OTP or Demo user.
+   *
+   * @param email Email to authenticate
+   */
+  login(hint?: {
+    email?: string;
+    userId?: string;
+    grant_type?: 'demo' | 'otp';
+  }): Promise<void>;
+  /**
+   * Connect to given URL
+   */
+  configure(options: DexieCloudOptions): void;
+  /** Wait until a full sync is done.
+   *
+   */
+  sync(options?: DexieCloudSyncOptions): Promise<void>;
+
+  permissions<T extends {owner: string, realmId: string, table: ()=>string}>(entity: T): Rx.Observable<PermissionChecker<T>>;
+  permissions(realmId: string, tableName: string, owner: string): Rx.Observable<PermissionChecker<any>>;
+}
 
 //
 // Extend Dexie interface
 //
 declare module 'dexie' {
   interface Dexie {
-    cloud: {
-      version: string;
-      options: DexieCloudOptions | null;
-      schema: DexieCloudSchema | null;
-      serverState: DexieCloudServerState | null;
-      currentUserId: string;
-      currentUser: Rx.BehaviorSubject<UserLogin>;
-      webSocketStatus: Rx.BehaviorSubject<DXCWebSocketStatus>;
-      syncState: Rx.BehaviorSubject<SyncState>;
-      persistedSyncState: Rx.BehaviorSubject<PersistedSyncState | undefined>;
-      userInteraction: Rx.BehaviorSubject<DXCUserInteraction | undefined>;
-      //loginState: Rx.BehaviorSubject<LoginState>;
-      usingServiceWorker?: boolean;
-      /** Login using Dexie Cloud OTP or Demo user.
-       *
-       * @param email Email to authenticate
-       */
-      login(hint?: {
-        email?: string;
-        userId?: string;
-        grant_type?: 'demo' | 'otp';
-      }): Promise<void>;
-      /**
-       * Connect to given URL
-       */
-      configure(options: DexieCloudOptions): void;
-      /** Wait until a full sync is done.
-       *
-       */
-      sync(options?: DexieCloudSyncOptions): Promise<void>;
-    };
+    cloud: DexieCloudAPI<this>;
+    realms: DexieCloudTable<DBRealm, string>;
+    members: DexieCloudTable<DBRealmMember, string>;
+    roles: DexieCloudTable<DBRealmRole, [string, string]>;
+  }
 
-    realms: Table<Partial<DBRealm>, string>;
-    members: Table<Partial<DBRealmMember>, string>;
-    roles: Table<DBRealmRole, [string, string]>;
+  interface Table<T, TKeyPropNameOrKeyType, TOpt> {
+    newId(colocateWith?: string): string;
   }
 
   interface DexieConstructor {
