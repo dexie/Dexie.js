d1eb1f0aee5b4b2469994f90aa43b7b7ac4aebcd
dfahlander <david.fahlander@gmail.com>
2021-11-28 17:01:20 +0100

    Adjust InsertType

--- src/public/types/insert-type.ts
+++ src/public/types/insert-type.ts
@@ -1,17 +1,19 @@
-import { Entity } from './entity';
-
 export type MethodProps<T> = {
   [P in keyof T]: T[P] extends (...args: any[]) => any ? P : never;
 }[keyof T];
 
-export type InsertType<T, TOptionalProperties> = string extends MethodProps<T>
+export type InsertType<T, TOptionalProperties, TKeyPropNameOrKeyType> = string extends MethodProps<T>
   ? T
   : Omit<
       T,
       TOptionalProperties extends keyof T
-        ? MethodProps<T> | TOptionalProperties
+        ? TKeyPropNameOrKeyType extends keyof T
+          ? MethodProps<T> | TOptionalProperties | TKeyPropNameOrKeyType
+          : MethodProps<T> | TOptionalProperties
         : MethodProps<T>
     > &
       (TOptionalProperties extends keyof T
-        ? { [P in TOptionalProperties]?: T[P] }
+        ? TKeyPropNameOrKeyType extends keyof T
+          ? { [P in (TOptionalProperties | TKeyPropNameOrKeyType)]?: T[P] }
+          : { [P in TOptionalProperties]?: T[P] }
         : {});
