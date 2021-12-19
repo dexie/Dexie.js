e865e80a3c05821db40ecc3c64157104d345b11e
dfahlander <david.fahlander@gmail.com>
2021-12-19 08:22:21 +0100

    Disable populating private IDs We need to refine them before using them by default.

--- samples/dexie-cloud-todo-app/src/components/TodoItemView.tsx
+++ samples/dexie-cloud-todo-app/src/components/TodoItemView.tsx
@@ -1,36 +1,38 @@
-import React, { useEffect, useState } from "react";
-import { db } from "../db";
-import { TodoItem } from "../db/TodoItem";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
-import { usePermissions } from "dexie-react-hooks";
+import React, { useEffect, useState } from 'react';
+import { db } from '../db';
+import { TodoItem } from '../db/TodoItem';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { usePermissions } from 'dexie-react-hooks';
 
 interface Props {
   item: TodoItem;
 }
 
 export function TodoItemView({ item }: Props) {
-  const can = usePermissions(db, "todoItems", item);
-  console.log("TodoItem", item.title, item.owner, item.realmId);
+  const can = usePermissions(db, 'todoItems', item);
+  console.log('TodoItem', item.title, item.owner, item.realmId);
   return (
-    <div className={"row " + (item.done ? "done" : "")}>
+    <div className={'row ' + (item.done ? 'done' : '')}>
       <div className="narrow">
         <input
           type="checkbox"
-          disabled={!can.update("done")}
+          disabled={!can.update('done')}
           checked={!!item.done}
-          onChange={ev =>
+          onChange={(ev) => {
             db.todoItems.update(item.id, {
-              done: ev.target.checked
-            })
-          }
+              done: ev.target.checked,
+            });
+          }}
         />
       </div>
       <div className="todo-item-text">{item.title}</div>
       <div className="todo-item-trash">
         <button
           disabled={!can.delete()}
-          onClick={() => db.todoItems.delete(item.id!)} title="Delete item">
+          onClick={() => db.todoItems.delete(item.id!)}
+          title="Delete item"
+        >
           <FontAwesomeIcon icon={faTrash} />
         </button>
       </div>
