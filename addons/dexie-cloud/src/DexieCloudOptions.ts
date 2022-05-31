bb1894078a881f2f1e6aca574df9eb5265fe09fc
dfahlander <david.fahlander@gmail.com>
2022-05-31 21:27:41 +0200

    Docs of DexieCloudOptions - same as website

--- addons/dexie-cloud/src/DexieCloudOptions.ts
+++ addons/dexie-cloud/src/DexieCloudOptions.ts
@@ -2,17 +2,49 @@ import Dexie, { Collection, Table } from 'dexie';
 import { TokenFinalResponse } from 'dexie-cloud-common';
 
 export interface PeriodicSyncOptions {
+  // The minimum interval time, in milliseconds, at which the service-worker's
+  // periodic sync should occur.
   minInterval?: number;
 }
 export interface DexieCloudOptions {
+  // URL to a database created with `npx dexie-cloud create`
   databaseUrl: string;
+
+  // Whether to require authentication or opt-in to it using db.cloud.login()
   requireAuth?: boolean;
+
+  // Whether to use service worker. Combine with registering your own service
+  // worker and import "dexie-cloud-addon/dist/modern/service-worker.min.js" from it.
   tryUseServiceWorker?: boolean;
-  customLoginGui?: boolean;
-  unsyncedTables?: string[];
+
+  // Optional customization of periodic sync.
+  // See https://developer.mozilla.org/en-US/docs/Web/API/PeriodicSyncManager/register
   periodicSync?: PeriodicSyncOptions;
+
+  // Disable default login GUI and replace it with your own by
+  // subscribing to the `db.cloud.userInteraction` observable and render its emitted data.
+  customLoginGui?: boolean;
+
+  // Array of table names that should be considered local-only and
+  // not be synced with Dexie Cloud
+  unsyncedTables?: string[];
+
+  // By default Dexie Cloud will suffix the cloud DB ID to your IndexedDB database name
+  // in order to ensure that the local database is uniquely tied to the remote one and
+  // will use another local database if databaseURL is changed or if dexieCloud addon
+  // is not being used anymoore.
+  //
+  // By setting this value to `false`, no suffix will be added to the database name and
+  // instead, it will use the exact name that is specified in the Dexie constructor, 
+  // without a suffix.
   nameSuffix?: boolean;
+
+  // Disable websocket connection
   disableWebSocket?: boolean;
+
+  // Provides a custom way of fetching the JWT tokens. This option
+  // can be used when integrating with custom authentication.
+  // See https://dexie.org/cloud/docs/db.cloud.configure()#fetchtoken
   fetchTokens?: (tokenParams: {
     public_key: string;
     hints?: { userId?: string; email?: string };
