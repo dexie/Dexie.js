a94dcd793f4adc6a971481dc36ff0717815607b4
dfahlander <david.fahlander@gmail.com>
2022-10-26 10:19:35 +0200

    Playing aroud with DexieCloudTable to make it bw compat

--- addons/dexie-cloud/src/DexieCloudTable.ts
+++ addons/dexie-cloud/src/DexieCloudTable.ts
@@ -1,6 +1,8 @@
-import { IndexableType, Table } from 'dexie';
+import { IndexableType, Table, Extract } from 'dexie';
 
+type Indexable<T> = T extends IndexableType ? T : string;
+type IDType<TKey, TEntity> = Indexable<Extract<TEntity, TKey>>;
 
-export type DexieCloudTable<T = any, TKey extends IndexableType = string> = Table<
-  T, TKey, 'realmId' | 'owner'
+export type DexieCloudTable<T = any, TKey extends string=string> = Table<
+  T, IDType<TKey, T>, Omit<T, TKey | 'realmId' | 'owner'> & {realmId?: string, owner?: string} & {[P in TKey]?: IDType<TKey, T>}
 >;
