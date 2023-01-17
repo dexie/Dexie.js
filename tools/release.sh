eb0054d4ca0e477d5e565bc63877cd436ec83d56
dfahlander <david.fahlander@gmail.com>
2023-01-17 11:42:56 +0100

    No package-lock.json as we've moved over to pnpm

--- tools/release.sh
+++ tools/release.sh
@@ -81,7 +81,7 @@ else
 fi
 
 update_version 'package.json' $next_version
-update_version 'package-lock.json' $next_version
+#update_version 'package-lock.json' $next_version
 pnpm install # Updates package-lock.json
 
 # Commit package.json change
