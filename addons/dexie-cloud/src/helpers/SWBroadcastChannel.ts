19cdf0b3a45d9d386edbfd331d07037246d63d82
dfahlander <david.fahlander@gmail.com>
2023-03-10 00:30:42 +0100

    Fixed more node errors that prevented nextjs SSR

--- addons/dexie-cloud/src/helpers/SWBroadcastChannel.ts
+++ addons/dexie-cloud/src/helpers/SWBroadcastChannel.ts
@@ -1,5 +1,6 @@
 const swHolder: { registration?: ServiceWorkerRegistration } = {};
-const swContainer = typeof self !== undefined && self.document && navigator.serviceWorker; // self.document is to verify we're not the SW ourself
+const swContainer = typeof self !== 'undefined' && self.document && // self.document is to verify we're not the SW ourself
+                    typeof navigator !== 'undefined' && navigator.serviceWorker; 
 if (swContainer)
   swContainer.ready.then(
     (registration) => (swHolder.registration = registration)
@@ -16,6 +17,12 @@ if (typeof self !== 'undefined' && 'clients' in self && !self.document) {
   });
 }
 
+/** This class is a fallback for browsers that lacks BroadcastChannel but have
+ * service workers (which is Safari versions 11.1 through 15.3).
+ * Safari 15.4 with BroadcastChannel was released on 2022-03-14.
+ * We might be able to remove this class in a near future as Safari < 15.4 is
+ * already very low in market share as of 2023-03-10.
+ */
 export class SWBroadcastChannel {
   name: string;
   constructor(name: string) {
--- addons/dexie-cloud/src/helpers/randomString.ts
+++ addons/dexie-cloud/src/helpers/randomString.ts
@@ -1,5 +1,15 @@
 export function randomString(bytes: number) {
   const buf = new Uint8Array(bytes);
-  crypto.getRandomValues(buf);
-  return btoa(String.fromCharCode.apply(null, buf));
+  if (typeof crypto !== 'undefined') {
+    crypto.getRandomValues(buf);
+  } else {
+    for (let i = 0; i < bytes; i++) buf[i] = Math.floor(Math.random() * 256);
+  }
+  if (typeof Buffer !== 'undefined' && Buffer.from) {
+    return Buffer.from(buf).toString('base64');
+  } else if (typeof btoa !== 'undefined') {
+    return btoa(String.fromCharCode.apply(null, buf));
+  } else {
+    throw new Error('No btoa or Buffer available');
+  }
 }
