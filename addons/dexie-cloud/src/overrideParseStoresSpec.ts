6708f4f94c88748f389cd876d33378fc8987b3e6
dfahlander <david.fahlander@gmail.com>
2023-04-17 22:50:20 +0200

    Merge indexes of dexie-cloud access control tables ...if also declared in application code.

--- addons/dexie-cloud/src/overrideParseStoresSpec.ts
+++ addons/dexie-cloud/src/overrideParseStoresSpec.ts
@@ -8,6 +8,43 @@ export function overrideParseStoresSpec(origFunc: Function, dexie: Dexie) {
       ...DEXIE_CLOUD_SCHEMA,
       ...stores,
     };
+    // Merge indexes of DEXIE_CLOUD_SCHEMA with stores
+    Object.keys(DEXIE_CLOUD_SCHEMA).forEach((tableName: keyof typeof DEXIE_CLOUD_SCHEMA) => {
+      const schemaSrc = storesClone[tableName];
+      // Verify that they don't try to delete a table that is needed for access control of Dexie Cloud
+      if (schemaSrc == null) {
+        // They try to delete one of the built-in schema tables.
+        throw new Error(`Cannot delete table ${tableName} as it is needed for access control of Dexie Cloud`);
+      }
+      // If not trying to override a built-in table, then we can skip this and continue to next table.
+      if (!stores[tableName]) {
+        // They haven't tried to declare this table. No need to merge indexes.
+        return; // Continue
+      }
+
+      // They have declared this table. Merge indexes in case they didn't declare all indexes we need.
+      const requestedIndexes = schemaSrc.split(',').map(spec => spec.trim());
+      const builtInIndexes = DEXIE_CLOUD_SCHEMA[tableName].split(',').map(spec => spec.trim());
+      const requestedIndexSet = new Set(requestedIndexes.map(index => index.replace(/([&*]|\+\+)/g, "")));
+      // Verify that primary key is unchanged
+      if (requestedIndexes[0] !== builtInIndexes[0]) {
+        // Primary key must match exactly
+        throw new Error(`Cannot override primary key of table ${tableName}. Please declare it as {${
+          tableName}: ${
+            JSON.stringify(DEXIE_CLOUD_SCHEMA[tableName])
+          }`);
+      }
+      // Merge indexes
+      for (let i=1; i<builtInIndexes.length; ++i) {
+        const builtInIndex = builtInIndexes[i];
+        if (!requestedIndexSet.has(builtInIndex.replace(/([&*]|\+\+)/g, ""))) {
+          // Add built-in index if not already requested
+          storesClone[tableName] += `,${builtInIndex}`;
+        }
+      }
+    });
+
+    // Populate dexie.cloud.schema
     const cloudSchema = dexie.cloud.schema || (dexie.cloud.schema = {});
     const allPrefixes = new Set<string>();
     Object.keys(storesClone).forEach(tableName => {
