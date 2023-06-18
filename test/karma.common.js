e65cea2ffce80ea6bbc2e59f23104b720355a4fb
David Fahlander <david.fahlander@gmail.com>
2023-06-18 22:08:13 +0200

    Fixing ghactions2 (#1749)
    Fully replaced travis with github actions, but still don't test the full matrix and still not migrated to LambdaTest.

--- test/karma.common.js
+++ test/karma.common.js
@@ -1,16 +1,98 @@
 /* Base karma configurations to require and extend from other karma.conf.js
-*/
-const karmaCommon = {
-    frameworks: [
-      'qunit'
-    ],
+ */
+let karmaCommon;
+const ltBrowsers = require('./karma.lambdatest.js').customLaunchers;
 
-    reporters: [
-      'mocha'
-    ],
+const webdriverConfig = {
+  hostname: 'hub.lambdatest.com',
+  port: 80,
+};
+
+const webdriverConfigMobile = {
+  hostname: 'mobile-hub.lambdatest.com',
+  port: 80,
+};
+
+if (process.env.LAMBDATEST) {
+  for (const key of Object.keys(ltBrowsers)) {
+    ltBrowsers[key].base = 'WebDriver';
+    if (ltBrowsers[key].isRealMobile) {
+      ltBrowsers[key].config = webdriverConfigMobile;
+      ltBrowsers[key].user = process.env.LT_USERNAME;
+      ltBrowsers[key].accessKey = process.env.LT_ACCESS_KEY;
+      ltBrowsers[key].tunnel = true;
+      ltBrowsers[key].console = true;
+      ltBrowsers[key].network = true;
+      ltBrowsers[key].tunnelName = process.env.LT_TUNNEL_NAME || 'jasmine';
+      ltBrowsers[key].pseudoActivityInterval = 5000; // 5000 ms heartbeat
+    } else {
+      ltBrowsers[key].config = webdriverConfig;
+      ltBrowsers[key]['LT:Options'].username = process.env.LT_USERNAME;
+      ltBrowsers[key]['LT:Options'].accessKey = process.env.LT_ACCESS_KEY;
+      ltBrowsers[key]['LT:Options'].tunnel = true;
+      ltBrowsers[key]['LT:Options'].console = true;
+      ltBrowsers[key]['LT:Options'].network = true;
+      ltBrowsers[key]['LT:Options'].tunnelName =
+        process.env.LT_TUNNEL_NAME || 'jasmine';
+      ltBrowsers[key]['LT:Options'].pseudoActivityInterval = 5000; // 5000 ms heartbeat
+    }
+
+    ltBrowsers[key].retryLimit = 2;
+  }
+
+  karmaCommon = {
+    hostname: 'localhost.lambdatest.com',
+    frameworks: ['qunit'],
+
+    reporters: ['mocha'],
 
     client: {
-      captureConsole: false
+      captureConsole: false,
+    },
+
+    colors: true,
+
+    browserNoActivityTimeout: 2 * 60 * 1000,
+    browserDisconnectTimeout: 15000,
+    processKillTimeout: 15000,
+    browserSocketTimeout: 20000,
+
+    plugins: [
+      'karma-qunit',
+      'karma-mocha-reporter',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-webdriver-launcher',
+    ],
+
+    files: [
+      'test/babel-polyfill/polyfill.min.js',
+      'node_modules/qunitjs/qunit/qunit.js',
+      'test/karma-env.js',
+      {
+        pattern: 'test/worker.js',
+        watched: true,
+        included: false,
+        served: true,
+      },
+      {
+        pattern: '!(node_modules|tmp)*/*.map',
+        watched: false,
+        included: false,
+        served: true,
+      },
+    ],
+
+    customLaunchers: ltBrowsers,
+  };
+} else {
+  karmaCommon = {
+    frameworks: ['qunit'],
+
+    reporters: ['mocha'],
+
+    client: {
+      captureConsole: false,
     },
 
     colors: true,
@@ -32,22 +114,40 @@ const karmaCommon = {
       'test/babel-polyfill/polyfill.min.js',
       'node_modules/qunitjs/qunit/qunit.js',
       'test/karma-env.js',
-      { pattern: 'test/worker.js', watched: true, included: false, served: true },
-      { pattern: '!(node_modules|tmp)*/*.map', watched: false, included: false, served: true}
+      {
+        pattern: 'test/worker.js',
+        watched: true,
+        included: false,
+        served: true,
+      },
+      {
+        pattern: '!(node_modules|tmp)*/*.map',
+        watched: false,
+        included: false,
+        served: true,
+      },
     ],
-    
+
     browserStack: require('./karma.browserstack.js').browserStack,
 
-    customLaunchers: require('./karma.browserstack.js').customLaunchers
-};
+    customLaunchers: require('./karma.browserstack.js').customLaunchers,
+  };
+}
 
-const browserSuiteToUse = process.env.GH_ACTIONS ? 'ciLocal' : process.env.NODE_ENV === 'release' ?
-  "pre_npm_publish" :
-  process.env.TRAVIS ?
-    isNaN(process.env.TRAVIS_PULL_REQUEST) && process.env.BROWSER_STACK_USERNAME ?
-      "ci" :              // CI pushs to master and browserstack credentials exists
-      "ciLocal" :         // CI pull request or has no browserstack credentials.
-  "local";                // Developer local machine
+const browserSuiteToUse = process.env.LAMBDATEST
+  ? 'ciLT'
+  : process.env.NODE_ENV === 'release'
+  ? 'pre_npm_publish'
+  : process.env.BROWSER_STACK_USERNAME
+  ? "ci"
+  : process.env.GH_ACTIONS
+  ? "ciLocal"
+  : 'local'; // Developer local machine
+
+if (browserSuiteToUse === 'ci') {
+  console.log('Username length', process.env.BROWSER_STACK_USERNAME.length);
+  console.log('Access key length', process.env.BROWSER_STACK_ACCESS_KEY.length);
+}
 
 const defaultBrowserMatrix = require('./karma.browsers.matrix');
 
@@ -56,13 +156,24 @@ const defaultBrowserMatrix = require('./karma.browsers.matrix');
  *  Map between browser suite and array of browser to test.
  * @param configOverrides {Object} configOverrides to the common template
  */
-function getKarmaConfig (browserMatrixOverrides, configOverrides) {
-  console.log("Browser-suite: " + browserSuiteToUse);
-  browserMatrixOverrides = Object.assign({}, defaultBrowserMatrix, browserMatrixOverrides);
+function getKarmaConfig(browserMatrixOverrides, configOverrides) {
+  console.log('Browser-suite: ' + browserSuiteToUse);
+  browserMatrixOverrides = Object.assign(
+    {},
+    defaultBrowserMatrix,
+    browserMatrixOverrides
+  );
   const browsers = browserMatrixOverrides[browserSuiteToUse];
-  console.log("Browsers to test: " + browsers.join(', '));
-  const finalConfig = Object.assign({}, karmaCommon, configOverrides, {browsers});
+  console.log('Browsers to test: ' + browsers.join(', '));
+  const finalConfig = Object.assign({}, karmaCommon, configOverrides, {
+    browsers,
+  });
   return finalConfig;
 }
 
-module.exports = {karmaCommon, getKarmaConfig, browserSuiteToUse, defaultBrowserMatrix};
+module.exports = {
+  karmaCommon,
+  getKarmaConfig,
+  browserSuiteToUse,
+  defaultBrowserMatrix,
+};
