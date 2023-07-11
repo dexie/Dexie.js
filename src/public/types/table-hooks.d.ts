0c36bd2ac87862aeea05adf2d526b40f52c42393
Nick <nlaurie@metafig.com>
2023-07-11 15:20:26 -0400

    Update to Recursive Type reference

--- src/public/types/table-hooks.d.ts
+++ src/public/types/table-hooks.d.ts
@@ -18,9 +18,9 @@ interface DeletingHookContext<T,Key> {
   onerror?: (err: any) => void;
 }
 
-interface TableHooks<T=any,TKey=IndexableType> extends DexieEventSet {
+interface TableHooks<T=any,TKey=IndexableType,TInsertType=T> extends DexieEventSet {
   (eventName: 'creating', subscriber: (this: CreatingHookContext<T,TKey>, primKey:TKey, obj:T, transaction:Transaction) => void | undefined | TKey): void;
-  (eventName: 'reading', subscriber: (obj:T) => T | any): void;
+  (eventName: 'reading', subscriber: (obj:TInsertType) => T | any): void;
   (eventName: 'updating', subscriber: (this: UpdatingHookContext<T,TKey>, modifications:Object, primKey:TKey, obj:T, transaction:Transaction) => any): void;
   (eventName: 'deleting', subscriber: (this: DeletingHookContext<T,TKey>, primKey:TKey, obj:T, transaction:Transaction) => any): void;
   creating: DexieEvent;
