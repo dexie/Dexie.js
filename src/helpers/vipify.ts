b75d6accff6c5862214dcc65c22ee92fdb6c948f
dfahlander <david.fahlander@gmail.com>
2023-07-15 17:55:37 +0200

    One unit test fails. Must use Proxy instead of Object.create() for vipified tables / transactions. This will makes uses less complex, especially places where code is setting props on the vipified objects without having to get adjusted for the vip case.

--- src/helpers/vipify.ts
+++ src/helpers/vipify.ts
@@ -2,9 +2,17 @@ import { type Dexie } from "../classes/dexie";
 import { type Table } from "../classes/table";
 import { type Transaction } from "../classes/transaction";
 
-export function vipify(
-  target: Table | Transaction,
+export function vipify<T extends Table | Transaction>(
+  target: T,
   vipDb: Dexie
-): Table {
-  return Object.create(target, {db: {value: vipDb}});
+): T {
+  return new Proxy(target, {
+    get (target, prop, receiver) {
+      // The "db" prop of the table or transaction is the only one we need to
+      // override. The rest of the props can be accessed from the original
+      // object.
+      if (prop === 'db') return vipDb;
+      return Reflect.get(target, prop, receiver);
+    }
+  });
 }
