f4c8c8cb071af6237c2dbffdc216733e04fe3af5
dfahlander <david.fahlander@gmail.com>
2023-12-11 15:50:00 +0100

    Upgrading browsers to test on

--- test/karma.lambdatest.js
+++ test/karma.lambdatest.js
@@ -2,28 +2,28 @@ module.exports = {
   customLaunchers: {
     remote_firefox: {
       browserName: 'firefox',
-      browserVersion: '114',
+      browserVersion: '118',
       'LT:Options': {
         platformName: 'Windows 10'
       }
     },
     remote_edge: {
       browserName: 'Edge',
-      browserVersion: '114',
+      browserVersion: '118',
       'LT:Options': {
         platformName: 'Windows 10'
       }
     },
     remote_chrome: {
       browserName: "Chrome",
-      browserVersion: "114",
+      browserVersion: "118",
       'LT:Options': {
         platformName: 'Windows 10'
       }
     },
     remote_safari: {
       browserName: "Safari",
-      browserVersion: "15",
+      browserVersion: "16",
       'LT:Options': {
         platformName: 'MacOS Monterey'
       }
