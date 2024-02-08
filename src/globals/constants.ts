ef134791d5a06b8751af5214e2415c95c4ecf11d
dfahlander <david.fahlander@gmail.com>
2024-02-09 00:19:28 +0100

    Remove workarounds for IE/old edge

--- src/globals/constants.ts
+++ src/globals/constants.ts
@@ -7,10 +7,6 @@ export const INVALID_KEY_ARGUMENT =
   "Invalid key provided. Keys must be of type string, number, Date or Array<string | number | Date>.";
 export const STRING_EXPECTED = "String expected.";
 export const connections: Dexie[] = [];
-export const isIEOrEdge =
-  typeof navigator !== 'undefined' && /(MSIE|Trident|Edge)/.test(navigator.userAgent);
-export const hasIEDeleteObjectStoreBug = isIEOrEdge;
-export const hangsOnDeleteLargeKeyRange = isIEOrEdge;
 export const dexieStackFrameFilter = frame => !/(dexie\.js|dexie\.min\.js)/.test(frame);
 export const DBNAMES_DB = '__dbnames';
 export const READONLY = 'readonly';
