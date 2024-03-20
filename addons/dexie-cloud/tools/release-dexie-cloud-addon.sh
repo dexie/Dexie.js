3265585ba135ce7f05249457e3a28dd03822f454
dfahlander <david.fahlander@gmail.com>
2024-03-20 13:06:18 +0100

    Releasing dexie-cloud-addon@4.0.1-rc.1

---
+++ addons/dexie-cloud/tools/release-dexie-cloud-addon.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+cd ../..
+pnpm install
+pnpm build
+cd libs/dexie-cloud-common
+pnpm install
+pnpm build
+cd ../../addons/dexie-cloud
+pnpm install
+pnpm build
+echo "All built. To publish, run 'pnpm publish [--tag test|next]'"
