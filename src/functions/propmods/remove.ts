4cfa6ec95f70237fcd6ebb8620c6afdba7639295
dfahlander <david.fahlander@gmail.com>
2024-07-02 00:31:05 +0200

    Use native bigint type instead of BigInt object type

--- src/functions/propmods/remove.ts
+++ src/functions/propmods/remove.ts
@@ -1,5 +1,5 @@
 import { PropModification } from "../../helpers/prop-modification";
 
-export function remove(value: number | BigInt | Array<string | number>) {
+export function remove(value: number | bigint | Array<string | number>) {
   return new PropModification({remove: value});
 }
