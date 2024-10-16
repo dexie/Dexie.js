92542c7e420a126e1a244d2c59482b1fe814dee4
David Fahlander <david@dexie.org>
2024-10-16 14:06:48 +0200

    Bugfix: some YJS updates were never sent to server

--- addons/dexie-cloud/src/yjs/createYClientUpdateObservable.ts
+++ addons/dexie-cloud/src/yjs/createYClientUpdateObservable.ts
@@ -1,10 +1,11 @@
-import { Observable, merge, mergeMap } from 'rxjs';
+import { Observable, from, merge, mergeMap, switchMap, tap } from 'rxjs';
 import { YClientMessage, YUpdateFromClientRequest } from 'dexie-cloud-common';
 import { DexieCloudDB } from '../db/DexieCloudDB';
 import { flatten } from '../helpers/flatten';
 import { liveQuery } from 'dexie';
 import { DEXIE_CLOUD_SYNCER_ID } from '../sync/DEXIE_CLOUD_SYNCER_ID';
 import { listUpdatesSince } from './listUpdatesSince';
+import { YDexieCloudSyncState } from './YDexieCloudSyncState';
 
 export function createYClientUpdateObservable(
   db: DexieCloudDB
@@ -25,39 +26,54 @@ export function createYClientUpdateObservable(
   );
   return merge(
     ...yTableRecords.map(({ table, ydocProp, updatesTable }) => {
-      let currentUnsentFrom = 1;
-      return liveQuery(async () => {
-        const yTbl = db.table(updatesTable);
-        const unsentFrom = await yTbl
-          .where({ i: DEXIE_CLOUD_SYNCER_ID })
-          .first()
-          .then((syncer) => syncer?.unsentFrom || 1);
-        currentUnsentFrom = Math.max(currentUnsentFrom, unsentFrom);
-        const addedUpdates = await listUpdatesSince(yTbl, currentUnsentFrom);
-        // Update currentUnsentFrom to only listen for updates that will be newer than the ones we emitted.
-        currentUnsentFrom = Math.max(
-          currentUnsentFrom,
-          ...addedUpdates.map((update) => update.i + 1)
-        );
-        return addedUpdates
-          .filter((update) => update.f && update.f & 1) // Only include local updates
-          .map((update) => {
-            return {
-              type: 'u-c',
-              table,
-              prop: ydocProp,
-              k: update.k,
-              u: update.u,
-              i: update.i,
-            } satisfies YUpdateFromClientRequest;
-          });
-      });
+      // Per updates table (table+prop combo), we first read syncer.unsentFrom,
+      // and then start listening for updates since that number.
+      const yTbl = db.table(updatesTable);
+      return from(yTbl.get(DEXIE_CLOUD_SYNCER_ID)).pipe(
+        switchMap((syncer: YDexieCloudSyncState) => {
+          let currentUnsentFrom = syncer?.unsentFrom || 1;
+          return from(
+            liveQuery(async () => {
+              const addedUpdates = await listUpdatesSince(
+                yTbl,
+                currentUnsentFrom
+              );
+              return addedUpdates
+                .filter((update) => update.f && update.f & 1) // Only include local updates
+                .map((update) => {
+                  return {
+                    type: 'u-c',
+                    table,
+                    prop: ydocProp,
+                    k: update.k,
+                    u: update.u,
+                    i: update.i,
+                  } satisfies YUpdateFromClientRequest;
+                });
+            })
+          ).pipe(
+            tap((addedUpdates) => {
+              // Update currentUnsentFrom to only listen for updates that will be newer than the ones we emitted.
+              // (Before, we did this within the liveQuery, but that caused a bug because
+              // a cancelled emittion of a liveQuery would update the currentUnsentFrom without
+              // emitting anything, leading to that we jumped over some updates. Here we update it
+              // after the liveQuery has emitted its updates)
+              if (addedUpdates.length > 0) {
+                currentUnsentFrom = addedUpdates.at(-1)!.i + 1;
+              }
+            })
+          );
+        })
+      );
     })
   ).pipe(
     // Flatten the array of messages.
     // If messageProducer emits empty array, nothing is emitted
     // but if messageProducer emits array of messages, they are
     // emitted one by one.
-    mergeMap((messages) => messages)
-  ); 
+    mergeMap((messages) => messages),
+    tap((message) => {
+      console.debug('dexie-cloud emitting y-c', message);
+    })
+  );
 }
