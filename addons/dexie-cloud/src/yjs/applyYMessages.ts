eefba6f121a6b49d36aad5bac153baa59deee286
David Fahlander <david@dexie.org>
2024-10-16 16:36:07 +0200

    When rejecting an update, reject all following updates as well from the same client, and destroy doc if it is open.

--- addons/dexie-cloud/src/yjs/applyYMessages.ts
+++ addons/dexie-cloud/src/yjs/applyYMessages.ts
@@ -1,4 +1,4 @@
-import { DexieYProvider, InsertType, YSyncState, YUpdateRow } from 'dexie';
+import { cmp, DexieYProvider, InsertType, YSyncState, YUpdateRow } from 'dexie';
 import { DexieCloudDB } from '../db/DexieCloudDB';
 import { YServerMessage } from 'dexie-cloud-common';
 import { DEXIE_CLOUD_SYNCER_ID } from '../sync/DEXIE_CLOUD_SYNCER_ID';
@@ -41,7 +41,23 @@ export async function applyYServerMessages(
         // See my question in https://discuss.yjs.dev/t/generate-an-inverse-update/2765
         console.debug(`Y update rejected. Deleting it.`);
         const utbl = getUpdatesTable(db, m.table, m.prop);
-        await utbl.delete(m.i);
+        // Delete the rejected update and all local updates since (avoid holes in the CRDT)
+        // and destroy it's open document if there is one.
+        const primaryKey = (await utbl.get(m.i))?.k;
+        if (primaryKey != null) {
+          await db.transaction('rw', utbl, tx => {
+            // @ts-ignore
+            tx.idbtrans._rejecting_y_ypdate = true; // Inform ydoc triggers that we delete because of a rejection and not GC
+            return utbl
+              .where('i')
+              .aboveOrEqual(m.i)
+              .filter(u => cmp(u.k, primaryKey) === 0 && ((u.f ||0) & 1) === 1)
+              .delete();
+          });
+          // Destroy active doc
+          const activeDoc = DexieYProvider.getDocCache(db.dx).find(m.table, primaryKey, m.prop);
+          if (activeDoc) activeDoc.destroy(); // Destroy the document so that editors don't continue to work on it
+        }
         break;
       }
       case 'in-sync': {
