d837a772be50100b4990159e7a0ea64ab11e90cb
David Fahlander <david.fahlander@gmail.com>
2025-07-31 16:37:07 +0200

    Typo

--- libs/dexie-react-hooks/test/gh-actions.sh
+++ libs/dexie-react-hooks/test/gh-actions.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 echo "Installing dependencies for dexie-react-hooks"
-pnpm install >/dev/null
 # Build y-dexie first as it is a devDependency for dexie-react-hooks
-cd ../addons/y-dexie
+cd ../../addons/y-dexie
 pnpm install >/dev/null
 pnpm run build
 cd -
+pnpm install >/dev/null
 echo "Building and running the tests"
 pnpm test:ltcloud
 
