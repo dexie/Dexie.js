39e219b339dc6316d247c5ef200b81abbb9c6767
David Fahlander <david.fahlander@gmail.com>
2025-07-31 16:10:15 +0200

    Build y-dexie before building dexie-react-hooks

--- libs/dexie-react-hooks/test/gh-actions.sh
+++ libs/dexie-react-hooks/test/gh-actions.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -e
 echo "Installing dependencies for dexie-react-hooks"
 pnpm install >/dev/null
+# Build y-dexie first as it is a devDependency for dexie-react-hooks
+cd ../addons/y-dexie
+pnpm install >/dev/null
+pnpm run build
+cd -
 echo "Building and running the tests"
 pnpm test:ltcloud
 
