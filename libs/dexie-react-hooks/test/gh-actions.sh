51c8d88b748b85fafdd7cf898aaa3a4cea921727
David Fahlander <david.fahlander@gmail.com>
2025-07-31 18:24:49 +0200

    expecting wrong current directory

--- libs/dexie-react-hooks/test/gh-actions.sh
+++ libs/dexie-react-hooks/test/gh-actions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 echo "Installing dependencies for dexie-react-hooks"
 # Build y-dexie first as it is a devDependency for dexie-react-hooks
-cd ../../addons/y-dexie
+cd ../../../addons/y-dexie
 pnpm install >/dev/null
 pnpm run build
 cd -
