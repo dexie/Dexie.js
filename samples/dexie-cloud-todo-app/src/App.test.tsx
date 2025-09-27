ce23f9237fd4a5febf80f3d2249ee2e1d17f41fb
David Fahlander <david.fahlander@gmail.com>
2025-09-27 14:55:33 +0200

    Test compiles and runs

--- samples/dexie-cloud-todo-app/src/App.test.tsx
+++ samples/dexie-cloud-todo-app/src/App.test.tsx
@@ -1,12 +1,13 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
 import App from './App';
 
 describe('App', () => {
-  it('renders todo app', () => {
+  it('renders todo app without crashing', () => {
     render(<App />);
     // Test that the navbar and app title renders
     const element = screen.getByText('Dexie Cloud ToDo App');
-    expect(element).toBeInTheDocument();
+    expect(element).toBeTruthy();
   });
 });
--- samples/dexie-cloud-todo-app/src/vite-env.d.ts
+++ samples/dexie-cloud-todo-app/src/vite-env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pwa/client" />
 /// <reference types="vitest/globals" />
+/// <reference types="@testing-library/jest-dom" />
 
 interface ImportMetaEnv {
   readonly VITE_DBURL: string
