5c79bb430615d4df7f79a44fed67edd3d81d9cd7
David Fahlander <david.fahlander@gmail.com>
2025-12-20 02:07:23 +0100

    Build output

---
+++ addons/dexie-export-import/dist/tson-arraybuffer.d.ts
@@ -0,0 +1,8 @@
+declare const _default: {
+    arraybuffer: {
+        test(x: any): boolean;
+        replace(b: any): string;
+        revive(b64: any): ArrayBuffer;
+    };
+};
+export default _default;
