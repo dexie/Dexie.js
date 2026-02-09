8920f3a3a74ccc6c832215d6d0a7f40de49395ed
Nikita <lisqxd@proton.me>
2026-02-09 14:38:58 +0100

    capture const

--- libs/dexie-react-hooks/src/useSuspendingObservable.ts
+++ libs/dexie-react-hooks/src/useSuspendingObservable.ts
@@ -98,7 +98,7 @@ export function useSuspendingObservable<T>(
             subscription = undefined;
             // Remove this observable from cache
             for (const [key, value] of observableCache) {
-              if (value === observable) {
+              if (value === newObservable) {
                 observableCache.delete(key);
                 break;
               }
