67a421f1fb92755215f6ea17f7fa3b181929598c
David Fahlander <david.fahlander@gmail.com>
2026-03-27 11:32:10 +0100

    fix: restore @ts-ignore placement in Disposable.ts after prettier reformat

--- addons/y-dexie/src/helpers/Disposable.ts
+++ addons/y-dexie/src/helpers/Disposable.ts
@@ -1,13 +1,14 @@
 export type Disposable = typeof Symbol extends {
   dispose: symbol;
 }
-  ? //@ts-ignore
+  ? // @ts-ignore
     { [Symbol.dispose]: () => void }
   : {};
 // @ts-ignore
 if (typeof Symbol.dispose !== 'symbol') {
   // @ts-ignore
   try {
+    // @ts-ignore
     Symbol.dispose = Symbol('dispose');
   } catch {}
 }
